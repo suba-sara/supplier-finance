e70a1a0f03565eb45a35cb0ba381928e6914bfd2
Shashi Srinath <atshashisrinath@gmail.com>
2021-04-26 18:34:42 +0530

    Initial commit

---
+++ capstone-server/src/main/java/com/hcl/capstoneserver/CapstoneServerApplication.java
@@ -0,0 +1,13 @@
+package com.hcl.capstoneserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CapstoneServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CapstoneServerApplication.class, args);
+    }
+
+}
---
+++ capstone-server/src/test/java/com/hcl/capstoneserver/CapstoneServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hcl.capstoneserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CapstoneServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
