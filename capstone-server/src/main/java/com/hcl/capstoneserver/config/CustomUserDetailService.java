9b84d0fc643099f6ff63f968fec69e6926d58a82
Shashi Srinath <atshashisrinath@gmail.com>
2021-04-26 22:58:03 +0530

    created custom user details and service

---
+++ capstone-server/src/main/java/com/hcl/capstoneserver/config/CustomUserDetailService.java
@@ -0,0 +1,31 @@
+package com.hcl.capstoneserver.config;
+
+import com.hcl.capstoneserver.user.entities.AppUser;
+import com.hcl.capstoneserver.user.repositories.AppUserRepository;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+@Service
+public class CustomUserDetailService implements UserDetailsService {
+
+    private final AppUserRepository appUserRepository;
+
+    public CustomUserDetailService(AppUserRepository appUserRepository) {
+        this.appUserRepository = appUserRepository;
+    }
+
+    // used in spring security don't change
+    @Override
+    public UserDetails loadUserByUsername(String s) throws UsernameNotFoundException {
+        AppUser user = appUserRepository.findByUsername(s);
+
+        if (user == null)
+            throw new UsernameNotFoundException("User not found");
+
+
+        return new CustomUserDetails(user);
+
+    }
+}
