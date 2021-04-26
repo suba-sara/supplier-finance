151587689cdec538a461236fb596cc65cc03a80f
Shashi Srinath <atshashisrinath@gmail.com>
2021-04-26 23:31:24 +0530

    removewd unwanted method from appuser repo

--- capstone-server/src/main/java/com/hcl/capstoneserver/user/repositories/AppUserRepository.java
+++ capstone-server/src/main/java/com/hcl/capstoneserver/user/repositories/AppUserRepository.java
@@ -6,6 +6,4 @@ import org.springframework.stereotype.Repository;
 
 @Repository
 public interface AppUserRepository extends JpaRepository<AppUser, String> {
-
-    AppUser findByUsername(String username);
 }
