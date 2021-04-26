9236ba22bac36dd8e08d7a51106b385ddd8726e5
Shashi Srinath <atshashisrinath@gmail.com>
2021-04-26 20:40:31 +0530

    added @Repository annotation to app user repository

--- capstone-server/src/main/java/com/hcl/capstoneserver/user/repositories/AppUserRepository.java
+++ capstone-server/src/main/java/com/hcl/capstoneserver/user/repositories/AppUserRepository.java
@@ -2,6 +2,8 @@ package com.hcl.capstoneserver.user.repositories;
 
 import com.hcl.capstoneserver.user.entities.AppUser;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface AppUserRepository extends JpaRepository<AppUser, String> {
 }
