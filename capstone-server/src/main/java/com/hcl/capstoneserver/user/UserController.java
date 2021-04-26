92e6e578c128654d16ef1b779b10b35647fd70ec
Shashi Srinath <atshashisrinath@gmail.com>
2021-04-26 20:30:40 +0530

    created UserController

---
+++ capstone-server/src/main/java/com/hcl/capstoneserver/user/UserController.java
@@ -0,0 +1,9 @@
+package com.hcl.capstoneserver.user;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.CrossOrigin;
+
+@CrossOrigin
+@Controller
+public class UserController {
+}
