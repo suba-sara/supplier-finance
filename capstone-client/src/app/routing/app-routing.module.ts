693394a133c34ea25bc014adeef7593a83dc660b
Shevan <fernandowkbst@gmail.com>
2021-04-27 20:26:27 +0530

    add forget page routing path

--- capstone-client/src/app/routing/app-routing.module.ts
+++ capstone-client/src/app/routing/app-routing.module.ts
@@ -4,12 +4,14 @@ import {SignInComponent} from '../sign-in/sign-in.component';
 import {SignUpClientComponent} from '../sign-up/sign-up-client.component';
 import {SignUpSupplierComponent} from '../sign-up/sign-up-supplier.component';
 import {HomeComponent} from '../home/home.component';
+import {ForgetComponent} from '../forget/forget.component';
 
 const routes: Routes = [
   {path: '', component: HomeComponent},
   {path: 'sign-in', component: SignInComponent},
   {path: 'sign-up/client', component: SignUpClientComponent},
   {path: 'sign-up/supplier', component: SignUpSupplierComponent},
+  {path: 'forget', component: ForgetComponent}
 ];
 
 @NgModule({
