6ab7970efe52e981a7122c598cf2f5bb5a446c78
Shevan <fernandowkbst@gmail.com>
2021-04-27 12:33:37 +0530

    import http client module

--- capstone-client/src/app/app.module.ts
+++ capstone-client/src/app/app.module.ts
@@ -1,19 +1,23 @@
-import { NgModule } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
+import {NgModule} from '@angular/core';
+import {BrowserModule} from '@angular/platform-browser';
 
-import { AppRoutingModule } from './routing/app-routing.module';
-import { AppComponent } from './app.component';
-import { SignUpClientComponent } from './sign-up/sign-up-client.component';
+import {AppRoutingModule} from './routing/app-routing.module';
+import {AppComponent} from './app.component';
+import {SignUpClientComponent} from './sign-up/sign-up-client.component';
 import {SignInComponent} from './sign-in/sign-in.component';
 import {SignUpSupplierComponent} from './sign-up/sign-up-supplier.component';
-import { TopNavbarComponent } from './top-navbar/top-navbar.component';
-import { HomeComponent } from './home/home.component';
+import {TopNavbarComponent} from './top-navbar/top-navbar.component';
+import {HomeComponent} from './home/home.component';
 
 // import angular materials
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
 import {MatInputModule} from '@angular/material/input';
 import {MatIconModule} from '@angular/material/icon';
 import {MatButtonModule} from '@angular/material/button';
+import {FormsModule} from '@angular/forms';
+
+// import httpClient
+import {HttpClientModule} from '@angular/common/http';
 
 @NgModule({
   declarations: [
@@ -30,9 +34,12 @@ import {MatButtonModule} from '@angular/material/button';
     BrowserAnimationsModule,
     MatInputModule,
     MatIconModule,
-    MatButtonModule
+    MatButtonModule,
+    FormsModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+}
