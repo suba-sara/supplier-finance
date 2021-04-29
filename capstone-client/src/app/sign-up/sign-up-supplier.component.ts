b8d19c469455901c08d8f5370ae284580fdb75c7
Hashen Weerapperuma <hashen.weerapperuma@gmail.com>
2021-04-29 18:15:35 +0530

    created two step signup form layout

--- capstone-client/src/app/sign-up/sign-up-supplier.component.ts
+++ capstone-client/src/app/sign-up/sign-up-supplier.component.ts
@@ -1,4 +1,23 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+
+type ClientDetail = {
+  firstName: string,
+  lastName: string,
+  email: string,
+  mobileNumber: string,
+  addressLine1: string,
+  city: string,
+  state: string,
+  province: string;
+  country: string,
+};
+type SupplierAccDetail = {
+  creditAccNumber?: number,
+  bankCode?: number,
+  supplierLimit?: number,
+  invoicePayment?: string
+
+};
 
 @Component({
   selector: 'app-sign-up-supplier',
@@ -6,10 +25,37 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./sign-up-supplier.component.scss']
 })
 export class SignUpSupplierComponent implements OnInit {
+  clientDetails: ClientDetail;
+  pageNumber: 1 | 2;
 
-  constructor() { }
+  supplierAccDetail: SupplierAccDetail;
+
+  constructor() {
+    this.clientDetails = {
+      firstName: '',
+      lastName: '',
+      email: '',
+      mobileNumber: '',
+      addressLine1: '',
+      city: '',
+      state: '',
+      province: '',
+      country: '',
+    };
+    this.pageNumber = 1;
+
+    this.supplierAccDetail = {};
+    this.pageNumber = 1;
+  }
 
   ngOnInit(): void {
   }
 
+  goToNextPage(): void {
+    this.pageNumber = 2;
+  }
+
+  goToPreviousPage(): void {
+    this.pageNumber = 1;
+  }
 }
