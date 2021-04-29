4e88443cca3f25d1de14ce65e2eb91951453fadf
Shashi Srinath <atshashisrinath@gmail.com>
2021-04-29 09:42:34 +0530

    modified signup response dto

--- capstone-server/src/main/java/com/hcl/capstoneserver/user/dto/SignUpResponseDTO.java
+++ capstone-server/src/main/java/com/hcl/capstoneserver/user/dto/SignUpResponseDTO.java
@@ -1,20 +1,34 @@
 package com.hcl.capstoneserver.user.dto;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+
 public class SignUpResponseDTO {
-    private String message;
+    private String username;
+    private String userType;
 
     public SignUpResponseDTO() {
     }
 
-    public SignUpResponseDTO(String message) {
-        this.message = message;
+    public SignUpResponseDTO(String username, String userType) {
+        this.username = username;
+        this.userType = userType;
     }
 
-    public String getMessage() {
-        return message;
+    public String getUsername() {
+        return username;
     }
 
-    public void setMessage(String message) {
-        this.message = message;
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getUserType() {
+        return userType;
+    }
+
+    public void setUserType(String userType) {
+        this.userType = userType;
     }
 }
