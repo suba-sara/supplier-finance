003690415772467182a2c766d8f1df0ed3508882
Shashi Srinath <atshashisrinath@gmail.com>
2021-04-29 09:43:24 +0530

    updated user service to check if user already exists

---
+++ capstone-server/src/main/java/com/hcl/capstoneserver/user/exceptions/UserAlreadyExistsException.java
@@ -0,0 +1,12 @@
+package com.hcl.capstoneserver.user.exceptions;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.client.HttpClientErrorException;
+
+@ResponseStatus(HttpStatus.FORBIDDEN)
+public class UserAlreadyExistsException extends HttpClientErrorException {
+    public UserAlreadyExistsException(String username) {
+        super(HttpStatus.FORBIDDEN, String.format("User with username: %s already exits", username));
+    }
+}
