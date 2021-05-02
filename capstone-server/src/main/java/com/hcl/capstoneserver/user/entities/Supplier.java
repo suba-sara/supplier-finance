fae7a0093b03fb1c4b38b652eac46618449e47a3
Shevan <fernandowkbst@gmail.com>
2021-05-02 13:13:11 +0530

    inherit the person class

--- capstone-server/src/main/java/com/hcl/capstoneserver/user/entities/Supplier.java
+++ capstone-server/src/main/java/com/hcl/capstoneserver/user/entities/Supplier.java
@@ -1,33 +1,21 @@
 package com.hcl.capstoneserver.user.entities;
 
-import com.hcl.capstoneserver.user.UserRole;
+import com.hcl.capstoneserver.user.UserType;
 
 import javax.persistence.Column;
 import javax.persistence.Entity;
 
 @Entity
-public class Supplier extends AppUser{
+public class Supplier extends Person {
     @Column(unique = true)
     private int supplierId;
-
-    private String name;
-    private String address;
-    private String email;
-    private String phone;
-    private Float interestRate;
 
     public Supplier() {
     }
 
-    public Supplier(String userId, String password, int supplierId,
-                    String name, String address, String email, String phone, Float interestRate) {
-        super(userId, password, "SUPPLIER");
+    public Supplier(String userId, String password, UserType userType, String name, String address, String email, String phone, Float interestRate, int supplierId) {
+        super(userId, password, userType, name, address, email, phone, interestRate);
         this.supplierId = supplierId;
-        this.name = name;
-        this.address = address;
-        this.email = email;
-        this.phone = phone;
-        this.interestRate = interestRate;
     }
 
     public int getSupplierId() {
@@ -38,43 +26,10 @@ public class Supplier extends AppUser{
         this.supplierId = supplierId;
     }
 
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public String getAddress() {
-        return address;
-    }
-
-    public void setAddress(String address) {
-        this.address = address;
-    }
-
-    public String getEmail() {
-        return email;
-    }
-
-    public void setEmail(String email) {
-        this.email = email;
-    }
-
-    public String getPhone() {
-        return phone;
-    }
-
-    public void setPhone(String phone) {
-        this.phone = phone;
-    }
-
-    public Float getInterestRate() {
-        return interestRate;
-    }
-
-    public void setInterestRate(Float interestRate) {
-        this.interestRate = interestRate;
+    @Override
+    public String toString() {
+        return "Supplier{" +
+                "supplierId=" + supplierId +
+                "} " + super.toString();
     }
 }
