89c6aa0bde1b9f5e243ebeefd490624423013974
Shashi Srinath <atshashisrinath@gmail.com>
2021-05-03 16:58:33 +0530

    created simple test cases for signup api

--- capstone-server/src/test/java/com/hcl/capstoneserver/user/UserServiceTest.java
+++ capstone-server/src/test/java/com/hcl/capstoneserver/user/UserServiceTest.java
@@ -1,13 +1,48 @@
 package com.hcl.capstoneserver.user;
 
+import com.hcl.capstoneserver.user.dto.SignUpSupplierRequestDTO;
+import com.hcl.capstoneserver.user.entities.Supplier;
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
+
 @SpringBootTest
 public class UserServiceTest {
-    @Autowired UserService userService;
+    @Autowired
+    UserService userService;
 
+    @Test
+    @DisplayName("it should create a new supplier")
+    public void createNewSupplier() {
+        Assertions.assertNotNull(userService.signUpSupplier(new SignUpSupplierRequestDTO(
+                "sup1",
+                "password",
+                "madara",
+                "konoha",
+                "madara@konoha.org",
+                "123456",
+                5.0F,
+                10002
+        )));
+    }
 
+    @Test
+    @DisplayName("It should generate a new supplier id")
+    public void shouldGenerateSupplierId() {
+        Supplier supplier = userService.signUpSupplier(new SignUpSupplierRequestDTO(
+                "sup2",
+                "password",
+                "madara",
+                "konoha",
+                "madara@konoha.org",
+                "123456",
+                5.0F,
+                10002
+        ));
+
+        Assertions.assertNotNull(supplier.getSupplierId());
+    }
 }
