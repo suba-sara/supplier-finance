f83ef19ba54c6e339a383c9fc3260cacf56f671e
Shashi Srinath <atshashisrinath@gmail.com>
2021-05-03 16:03:52 +0530

    created user service test class

---
+++ capstone-server/src/test/java/com/hcl/capstoneserver/user/UserServiceTest.java
@@ -0,0 +1,13 @@
+package com.hcl.capstoneserver.user;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+public class UserServiceTest {
+    @Autowired UserService userService;
+
+
+}
