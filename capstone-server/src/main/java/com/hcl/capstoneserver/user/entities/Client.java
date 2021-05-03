2067b037c5be2dfcb5ae30cf0a0bb3a272bcc8fd
Shevan <fernandowkbst@gmail.com>
2021-05-03 11:44:11 +0530

    add custom id generator

--- capstone-server/src/main/java/com/hcl/capstoneserver/user/entities/Client.java
+++ capstone-server/src/main/java/com/hcl/capstoneserver/user/entities/Client.java
@@ -1,13 +1,29 @@
 package com.hcl.capstoneserver.user.entities;
 
+import com.hcl.capstoneserver.generator.id.CustomIdGenerator;
 import com.hcl.capstoneserver.user.UserType;
+import org.hibernate.annotations.GenericGenerator;
+import org.hibernate.annotations.Parameter;
 
 import javax.persistence.Column;
 import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
 
 @Entity
 public class Client extends Person {
-
+    @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "client_id_sequence")
+    @GenericGenerator(name = "client_id_sequence",
+            strategy = "com.hcl.capstoneserver.generator.id.CustomIdGenerator",
+            parameters = {
+                    @Parameter(name = CustomIdGenerator.SEQUENCE_PARAM, value = "client_id_sequence"),
+                    @Parameter(name = CustomIdGenerator.INITIAL_PARAM, value = "1"),
+                    @Parameter(name = CustomIdGenerator.OPT_PARAM, value = "pooled-lo"),
+                    @Parameter(name = CustomIdGenerator.INCREMENT_PARAM, value = "1"),
+                    @Parameter(name = CustomIdGenerator.PREFIX_PARAM, value = "CL_"),
+                    @Parameter(name = CustomIdGenerator.NUMBER_FORMAT_PARAM, value = "%05d")
+            }
+    )
     @Column(unique = true)
     private int clientId;
 
