40f033ec14c6f24505f96c5045b05326c1d1be93
Shashi Srinath <atshashisrinath@gmail.com>
2021-05-04 22:38:43 +0530

    added some validations

--- capstone-client/src/app/sign-up/sign-up-supplier.component.ts
+++ capstone-client/src/app/sign-up/sign-up-supplier.component.ts
@@ -1,32 +1,74 @@
-import {Component, OnInit} from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import {
+  FormControl,
+  FormGroup,
+  ValidationErrors,
+  Validators,
+} from '@angular/forms';
 
 type ClientDetail = {
-  firstName: string,
-  lastName: string,
-  email: string,
-  mobileNumber: string,
-  addressLine1: string,
-  city: string,
-  state: string,
+  firstName: string;
+  lastName: string;
+  email: string;
+  mobileNumber: string;
+  addressLine1: string;
+  city: string;
+  state: string;
   province: string;
-  country: string,
+  country: string;
 };
 type SupplierAccDetail = {
-  creditAccNumber?: number,
-  bankCode?: number,
-  supplierLimit?: number,
-  invoicePayment?: string
-
+  creditAccNumber?: number;
+  bankCode?: number;
+  supplierLimit?: number;
+  invoicePayment?: string;
 };
 
 @Component({
   selector: 'app-sign-up-supplier',
   templateUrl: './sign-up-supplier.component.html',
-  styleUrls: ['./sign-up-supplier.component.scss']
+  styleUrls: ['./sign-up-supplier.component.scss'],
 })
 export class SignUpSupplierComponent implements OnInit {
   clientDetails: ClientDetail;
   pageNumber: number;
+
+  clientFormData = new FormGroup(
+    {
+      firstName: new FormControl('', [Validators.required]),
+      lastName: new FormControl('', [Validators.required]),
+      email: new FormControl('', [Validators.required, Validators.email]),
+      phone: new FormControl('', [
+        Validators.required,
+        Validators.pattern(/^[0-9+ ]+$/),
+      ]),
+      addressLine1: new FormControl('', [Validators.required]),
+      city: new FormControl('', [Validators.required]),
+      state: new FormControl('', [Validators.required]),
+      province: new FormControl('', [Validators.required]),
+      country: new FormControl('', [Validators.required]),
+    },
+    {
+      validators: [
+        // full name validation function
+        (control): ValidationErrors | null => {
+          const firstName: string = control.get('firstName')?.value;
+          const lastName: string = control.get('lastName')?.value;
+
+          if (
+            firstName.length + lastName.length < 3 ||
+            firstName.length + lastName.length > 30
+          ) {
+            return {
+              fullName: true,
+            };
+          }
+
+          return null;
+        },
+      ],
+    }
+  );
 
   supplierAccDetail: SupplierAccDetail;
 
@@ -48,8 +90,7 @@ export class SignUpSupplierComponent implements OnInit {
     this.pageNumber = 1;
   }
 
-  ngOnInit(): void {
-  }
+  ngOnInit(): void {}
 
   goToPreviousPage(): void {
     if (this.pageNumber > 1) {
