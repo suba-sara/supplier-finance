ec896bd9efc9a366626bcc2d825a5d9ddcee860f
Shashi Srinath <atshashisrinath@gmail.com>
2021-05-04 13:56:45 +0530

    converted jwtutil to singleton

--- capstone-server/src/main/java/com/hcl/capstoneserver/util/JWTUtil.java
+++ capstone-server/src/main/java/com/hcl/capstoneserver/util/JWTUtil.java
@@ -5,6 +5,7 @@ import io.jsonwebtoken.Jwts;
 import io.jsonwebtoken.SignatureAlgorithm;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.stereotype.Component;
 import org.springframework.stereotype.Service;
 
 import java.util.Date;
@@ -12,7 +13,7 @@ import java.util.HashMap;
 import java.util.Map;
 import java.util.function.Function;
 
-@Service
+@Component
 public class JWTUtil {
     @Value("${jwt.secret}")
     private String SECRET_KEY;
