3658d73766b6cc3dfb7380692ac9d11ac4d5b4a5
Shashi Srinath <atshashisrinath@gmail.com>
2021-05-06 18:38:18 +0530

    make home page mobile responsive and added a new bg image to match colors

--- capstone-client/src/app/app.module.ts
+++ capstone-client/src/app/app.module.ts
@@ -1,36 +1,33 @@
-import {NgModule} from '@angular/core';
-import {BrowserModule} from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
 
-import {AppRoutingModule} from './routing/app-routing.module';
-import {AppComponent} from './app.component';
-import {SignInComponent} from './sign-in/sign-in.component';
-import {TopNavbarComponent} from './top-navbar/top-navbar.component';
-import {HomeComponent} from './home/home.component';
-import {ForgotPasswordComponent} from './forgot-password/forgot-password.component';
+import { AppRoutingModule } from './routing/app-routing.module';
+import { AppComponent } from './app.component';
+import { SignInComponent } from './sign-in/sign-in.component';
+import { HomeComponent } from './home/home.component';
+import { ForgotPasswordComponent } from './forgot-password/forgot-password.component';
 
 // import angular materials
-import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
-import {MatInputModule} from '@angular/material/input';
-import {MatIconModule} from '@angular/material/icon';
-import {MatButtonModule} from '@angular/material/button';
-import {FormsModule, ReactiveFormsModule} from '@angular/forms';
-import {MatCardModule} from '@angular/material/card';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MatInputModule } from '@angular/material/input';
+import { MatIconModule } from '@angular/material/icon';
+import { MatButtonModule } from '@angular/material/button';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { MatCardModule } from '@angular/material/card';
 
 // import httpClient
-import {HttpClientModule} from '@angular/common/http';
-import {DashboardComponent} from './dashboard/dashboard.component';
-import {SignUpModule} from './sign-up/sign-up.module';
-import {SignUpComponent} from './sign-up/sign-up.component';
+import { HttpClientModule } from '@angular/common/http';
+import { DashboardComponent } from './dashboard/dashboard.component';
+import { SignUpModule } from './sign-up/sign-up.module';
+import { TopNavbarModule } from './top-navbar/top-navbar.module';
 
 @NgModule({
   declarations: [
     AppComponent,
     SignInComponent,
-    TopNavbarComponent,
     HomeComponent,
     ForgotPasswordComponent,
     DashboardComponent,
-    SignUpComponent
   ],
   imports: [
     BrowserModule,
@@ -44,9 +41,9 @@ import {SignUpComponent} from './sign-up/sign-up.component';
     ReactiveFormsModule,
     MatCardModule,
     SignUpModule,
+    TopNavbarModule,
   ],
   providers: [],
   bootstrap: [AppComponent],
 })
-export class AppModule {
-}
+export class AppModule {}
