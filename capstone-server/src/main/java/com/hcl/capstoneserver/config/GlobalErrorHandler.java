3372f3234b8c830bbf7e3cb6ed8b93921afb67ea
Shashi Srinath <atshashisrinath@gmail.com>
2021-05-11 10:08:41 +0530

    updated global error handler

--- capstone-server/src/main/java/com/hcl/capstoneserver/config/GlobalErrorHandler.java
+++ capstone-server/src/main/java/com/hcl/capstoneserver/config/GlobalErrorHandler.java
@@ -2,28 +2,36 @@ package com.hcl.capstoneserver.config;
 
 import com.hcl.capstoneserver.config.error_responses.DefaultErrorResponse;
 import com.hcl.capstoneserver.config.error_responses.DefaultValidationErrorResponse;
+import io.jsonwebtoken.ExpiredJwtException;
+import io.jsonwebtoken.JwtException;
+import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.http.converter.HttpMessageNotReadableException;
+import org.springframework.security.authentication.BadCredentialsException;
 import org.springframework.validation.FieldError;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.client.HttpClientErrorException;
+import org.springframework.web.context.request.WebRequest;
+import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
 
 import java.util.ArrayList;
 import java.util.LinkedHashMap;
 import java.util.Map;
 
 @ControllerAdvice
-public class GlobalErrorHandler {
-    @ExceptionHandler(MethodArgumentNotValidException.class)
-    public final ResponseEntity<DefaultValidationErrorResponse> handleValidationArgumentException(MethodArgumentNotValidException ex) {
+public class GlobalErrorHandler extends ResponseEntityExceptionHandler {
+    @Override
+    protected ResponseEntity<Object> handleMethodArgumentNotValid(MethodArgumentNotValidException ex,
+                                                                  HttpHeaders headers, HttpStatus status,
+                                                                  WebRequest request) {
         ArrayList<Map<String, String>> errors = new ArrayList<>();
 
         for (FieldError fieldError : ex.getBindingResult().getFieldErrors()) {
             LinkedHashMap<String, String> error = new LinkedHashMap<>();
-            error.put("field",fieldError.getField());
+            error.put("field", fieldError.getField());
             error.put("message", fieldError.getDefaultMessage());
 
             errors.add(error);
@@ -31,6 +39,16 @@ public class GlobalErrorHandler {
 
         return new ResponseEntity<>(
                 new DefaultValidationErrorResponse(HttpStatus.BAD_REQUEST, "Validation Failed", errors),
+                HttpStatus.BAD_REQUEST
+        );
+    }
+
+    @Override
+    protected ResponseEntity<Object> handleHttpMessageNotReadable(HttpMessageNotReadableException ex,
+                                                                  HttpHeaders headers, HttpStatus status,
+                                                                  WebRequest request) {
+        return new ResponseEntity<>(
+                new DefaultErrorResponse(HttpStatus.BAD_REQUEST, "Invalid request"),
                 HttpStatus.BAD_REQUEST
         );
     }
@@ -43,13 +61,14 @@ public class GlobalErrorHandler {
         );
     }
 
-    @ExceptionHandler(HttpMessageNotReadableException.class)
-    public final ResponseEntity<DefaultErrorResponse> handleInvalidArguments(HttpMessageNotReadableException ex) {
+    @ExceptionHandler(BadCredentialsException.class)
+    public final ResponseEntity<DefaultErrorResponse> handleBadCredentials(BadCredentialsException ex) {
         return new ResponseEntity<>(
-                new DefaultErrorResponse(HttpStatus.BAD_REQUEST, "Invalid request"),
-                HttpStatus.BAD_REQUEST
+                new DefaultErrorResponse(HttpStatus.UNAUTHORIZED, "Invalid username or password"),
+                HttpStatus.UNAUTHORIZED
         );
     }
+
 
     @ExceptionHandler(HttpClientErrorException.class)
     public final ResponseEntity<DefaultErrorResponse> handleClientErrors(HttpClientErrorException ex) {
@@ -59,12 +78,11 @@ public class GlobalErrorHandler {
         );
     }
 
-    @ExceptionHandler(Exception.class)
-    public final ResponseEntity<DefaultErrorResponse> unhandledErrors(Exception ex) {
-        ex.printStackTrace();
+    @ExceptionHandler(JwtException.class)
+    protected ResponseEntity<Object> handleExpiredJwtException(JwtException ex) {
         return new ResponseEntity<>(
-                new DefaultErrorResponse(HttpStatus.INTERNAL_SERVER_ERROR, "INTERNAL SERVER ERROR"),
-                HttpStatus.INTERNAL_SERVER_ERROR
+                new DefaultErrorResponse(HttpStatus.UNAUTHORIZED, ex.getMessage()),
+                HttpStatus.UNAUTHORIZED
         );
     }
 }
