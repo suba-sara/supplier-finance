0758df21e2a0866a43aba74d19939242f5a60e6f
Shashi Srinath <atshashisrinath@gmail.com>
2021-05-11 10:15:07 +0530

    updated top-navbar module

--- capstone-client/src/app/core/top-navbar/top-navbar.module.ts
+++ capstone-client/src/app/core/top-navbar/top-navbar.module.ts
@@ -3,17 +3,12 @@ import { CommonModule } from '@angular/common';
 import { TopNavbarComponent } from './top-navbar.component';
 import { RouterModule } from '@angular/router';
 import { NavLinkComponent } from './nav-link/nav-link.component';
-import { LogoComponent } from './logo/logo.component';
 import { BurgerMenuComponent } from './burger-menu/burger-menu.component';
+import { SharedModule } from '../../shared/shared.module';
 
 @NgModule({
-  declarations: [
-    TopNavbarComponent,
-    NavLinkComponent,
-    LogoComponent,
-    BurgerMenuComponent,
-  ],
-  imports: [CommonModule, RouterModule],
+  declarations: [TopNavbarComponent, NavLinkComponent, BurgerMenuComponent],
+  imports: [CommonModule, RouterModule, SharedModule],
   exports: [TopNavbarComponent],
 })
 export class TopNavbarModule {}
