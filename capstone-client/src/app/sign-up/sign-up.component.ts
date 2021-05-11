4049ac14d8b29d2b3c05f31084a4fb1fadaf03a6
Shashi Srinath <atshashisrinath@gmail.com>
2021-05-11 10:33:31 +0530

    refactored
    Signed-off-by: Shashi Srinath <atshashisrinath@gmail.com>

--- capstone-client/src/app/sign-up/sign-up.component.ts
+++ capstone-client/src/app/sign-up/sign-up.component.ts
@@ -1,39 +1,12 @@
-import {Component, OnInit} from '@angular/core';
-import {AccountType} from './user-type-button/user-type-button.component';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
-    selector: 'app-sign-up',
-    templateUrl: './sign-up.component.html',
-    styleUrls: ['./sign-up.component.scss'],
+  selector: 'app-sign-up',
+  templateUrl: './sign-up.component.html',
+  styleUrls: ['./sign-up.component.scss'],
 })
 export class SignUpComponent implements OnInit {
-    client: AccountType;
-    supplier: AccountType;
+  constructor() {}
 
-    constructor() {
-        this.client = {
-            accountName: 'Client',
-            redirectPath: '/sign-up/client',
-            imagePath: '../../assets/Images/Client.png',
-            accountInfo: ['Lorem ipsum dolor sit amet, consectetur.',
-                'Lorem ipsum dolor sit amet, consectetur.',
-                'Lorem ipsum dolor sit amet, consectetur.',
-                'Lorem ipsum dolor sit amet, consectetur.',
-                'Lorem ipsum dolor sit amet, consectetur.']
-        };
-
-        this.supplier = {
-            accountName: 'Supplier',
-            redirectPath: '/sign-up/supplier',
-            imagePath: '../../assets/Images/Supplier.png',
-            accountInfo: ['Lorem ipsum dolor sit amet, consectetur.',
-                'Lorem ipsum dolor sit amet, consectetur.',
-                'Lorem ipsum dolor sit amet, consectetur.',
-                'Lorem ipsum dolor sit amet, consectetur.',
-                'Lorem ipsum dolor sit amet, consectetur.']
-        };
-    }
-
-    ngOnInit(): void {
-    }
+  ngOnInit(): void {}
 }
--- capstone-client/src/app/sign-up/user-type-button/user-type-button.component.ts
+++ capstone-client/src/app/sign-up/user-type-button/user-type-button.component.ts
@@ -1,25 +1,16 @@
-import {Component, Input, OnInit} from '@angular/core';
-
-export type AccountType = {
-    accountName: string,
-    redirectPath: string,
-    imagePath: string,
-    accountInfo: string[]
-};
+import { Component, Input, OnInit } from '@angular/core';
 
 @Component({
-    selector: 'app-user-type-button',
-    templateUrl: './user-type-button.component.html',
-    styleUrls: ['./user-type-button.component.scss']
+  selector: 'app-user-type-button',
+  templateUrl: './user-type-button.component.html',
+  styleUrls: ['./user-type-button.component.scss'],
 })
 export class UserTypeButtonComponent implements OnInit {
+  @Input() accountType!: string;
+  @Input() routePath!: string;
+  @Input() imagePath!: string;
 
-    @Input() accountType?: AccountType;
+  constructor() {}
 
-    constructor() {
-    }
-
-    ngOnInit(): void {
-    }
-
+  ngOnInit(): void {}
 }
