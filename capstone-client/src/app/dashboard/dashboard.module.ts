fe21ea8a159166fb1bb186f9392a5d08b6ca6843
Shashi Srinath <atshashisrinath@gmail.com>
2021-05-11 10:16:57 +0530

    updated dashboard layout

--- capstone-client/src/app/dashboard/dashboard.module.ts
+++ capstone-client/src/app/dashboard/dashboard.module.ts
@@ -1,9 +1,23 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { DashboardComponent } from './dashboard.component';
+import { MatIconModule } from '@angular/material/icon';
+import { MatButtonModule } from '@angular/material/button';
+import { MatTooltipModule } from '@angular/material/tooltip';
+import { RouterModule } from '@angular/router';
+import { SharedModule } from '../shared/shared.module';
+import { LeftNavbarModule } from '../core/left-navbar/left-navbar.module';
 
 @NgModule({
   declarations: [DashboardComponent],
-  imports: [CommonModule],
+  imports: [
+    CommonModule,
+    MatIconModule,
+    MatButtonModule,
+    MatTooltipModule,
+    RouterModule,
+    SharedModule,
+    LeftNavbarModule,
+  ],
 })
 export class DashboardModule {}
