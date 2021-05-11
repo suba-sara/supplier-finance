5a4a64adc40957b396c58a326b38e140c8198264
Shashi Srinath <atshashisrinath@gmail.com>
2021-05-11 10:11:44 +0530

    created title panel component

--- capstone-client/src/app/shared/shared.module.ts
+++ capstone-client/src/app/shared/shared.module.ts
@@ -1,8 +1,12 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { LogoComponent } from './logo/logo.component';
+import { RouterModule } from '@angular/router';
+import { TitlePanelComponent } from './title-panel/title-panel.component';
 
 @NgModule({
-  declarations: [],
-  imports: [CommonModule],
+  declarations: [LogoComponent, TitlePanelComponent],
+  imports: [CommonModule, RouterModule],
+  exports: [LogoComponent, TitlePanelComponent],
 })
 export class SharedModule {}
---
+++ capstone-client/src/app/shared/title-panel/title-panel.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-title-panel',
+  templateUrl: './title-panel.component.html',
+  styleUrls: ['./title-panel.component.scss']
+})
+export class TitlePanelComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
