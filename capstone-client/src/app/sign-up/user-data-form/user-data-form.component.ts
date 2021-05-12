890bec9ade43ebafda342a2fe0970f9e28b70e67
Hashen Weerapperuma <hashen.weerapperuma@gmail.com>
2021-05-12 09:33:37 +0530

    Create user data form

--- capstone-client/src/app/sign-up/user-data-form/user-data-form.component.ts
+++ capstone-client/src/app/sign-up/user-data-form/user-data-form.component.ts
@@ -1,4 +1,11 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, EventEmitter, OnInit, Output } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
+
+export type UserDetails = {
+  username: string;
+  password: string;
+  confirm_password: string;
+};
 
 @Component({
   selector: 'app-user-data-form',
@@ -6,7 +13,25 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./user-data-form.component.scss'],
 })
 export class UserDataFormComponent implements OnInit {
-  constructor() {}
+  userDataForm = new FormGroup({
+    username: new FormControl('', [Validators.required]),
+    password: new FormControl('', [
+      Validators.required,
+      Validators.minLength(8),
+    ]),
+    confirm_password: new FormControl('', [Validators.required]),
+  });
+
+  @Output()
+  formSubmitEvent = new EventEmitter<UserDetails>();
 
   ngOnInit(): void {}
+
+  confirmUserData(): void {
+    if (this.userDataForm.valid) {
+      this.formSubmitEvent.emit(this.userDataForm.value);
+    } else {
+      this.userDataForm.markAsDirty();
+    }
+  }
 }
