18b23adfc31f88d95f13ac54672bbf5492b8caf7
Shevan <w.k.b.s.t.fernando@gmail.com>
2021-05-13 10:47:49 +0530

    change the httpStatus forbidden to bad request

--- capstone-server/src/main/java/com/hcl/capstoneserver/user/exceptions/UserAlreadyExistsException.java
+++ capstone-server/src/main/java/com/hcl/capstoneserver/user/exceptions/UserAlreadyExistsException.java
@@ -4,9 +4,9 @@ import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.client.HttpClientErrorException;
 
-@ResponseStatus(HttpStatus.FORBIDDEN)
+@ResponseStatus(HttpStatus.BAD_REQUEST)
 public class UserAlreadyExistsException extends HttpClientErrorException {
     public UserAlreadyExistsException(String username) {
-        super(HttpStatus.FORBIDDEN, String.format("User with username %s already exits", username));
+        super(HttpStatus.BAD_REQUEST, String.format("User with username %s already exits", username));
     }
 }
