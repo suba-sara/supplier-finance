da87edaf05d6328adca8b6b70bc10877dd2850e7
Shevan <w.k.b.s.t.fernando@gmail.com>
2021-05-13 14:55:10 +0530

    add custom method for find the userEmail is existed or not

--- capstone-server/src/main/java/com/hcl/capstoneserver/user/repositories/SupplierRepository.java
+++ capstone-server/src/main/java/com/hcl/capstoneserver/user/repositories/SupplierRepository.java
@@ -6,6 +6,6 @@ import org.springframework.data.jpa.repository.Query;
 
 public interface SupplierRepository extends JpaRepository<Supplier, String> {
 
-    @Query("SELECT CASE WHEN COUNT(s)>0 THEN true ELSE false END FROM Supplier AS s WHERE s.email = ?1 ")
+    @Query("SELECT CASE WHEN COUNT(s)>0 THEN true ELSE false END FROM Supplier AS s WHERE s.email = ?1")
     Boolean existsByEmail(String email);
 }
