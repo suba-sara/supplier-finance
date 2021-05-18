6698362de0804b04217f70049096e1c4949eac97
Shashi Srinath <atshashisrinath@gmail.com>
2021-05-18 16:13:31 +0530

    fix import issue in tests
    Signed-off-by: Shashi Srinath <atshashisrinath@gmail.com>

--- capstone-client/src/app/sign-up/success-feedback/success-feedback.component.spec.ts
+++ capstone-client/src/app/sign-up/success-feedback/success-feedback.component.spec.ts
@@ -1,6 +1,7 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { SuccessFeedbackComponent } from './success-feedback.component';
+import { RouterTestingModule } from '@angular/router/testing';
 
 describe('SuccessFeedbackComponent', () => {
   let component: SuccessFeedbackComponent;
@@ -9,6 +10,7 @@ describe('SuccessFeedbackComponent', () => {
   beforeEach(async () => {
     await TestBed.configureTestingModule({
       declarations: [SuccessFeedbackComponent],
+      imports: [RouterTestingModule],
     }).compileComponents();
   });
 
