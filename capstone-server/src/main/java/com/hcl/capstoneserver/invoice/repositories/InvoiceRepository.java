c28cba1d36f38df70364b8fa4ce0466ed43ff682
Shevan Fernando <81354288+shevanfernando@users.noreply.github.com>
2021-05-24 11:52:28 +0530

    develop InvoiceRepository

--- capstone-server/src/main/java/com/hcl/capstoneserver/invoice/repositories/InvoiceRepository.java
+++ capstone-server/src/main/java/com/hcl/capstoneserver/invoice/repositories/InvoiceRepository.java
@@ -1,12 +1,10 @@
-package com.hcl.capstoneserver.invoice;
+package com.hcl.capstoneserver.invoice.repositories;
 
+import com.hcl.capstoneserver.invoice.entities.Invoice;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface InvoiceRepository extends JpaRepository<Invoice, Integer> {
-
-    @Query("SELECT i FROM Invoice i WHERE i.invoiceNumber = ?1")
-    boolean findInvoiceByInvoiceNumber(Integer invoiceNumber);
 }
