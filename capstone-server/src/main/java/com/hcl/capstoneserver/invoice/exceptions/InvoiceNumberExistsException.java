50e092ad7bd3610525d06f7f39ec2f5605042266
Shevan Fernando <81354288+shevanfernando@users.noreply.github.com>
2021-05-24 11:59:50 +0530

    develop InvoiceNumberExistsException

--- capstone-server/src/main/java/com/hcl/capstoneserver/invoice/exceptions/InvoiceNumberExistsException.java
+++ capstone-server/src/main/java/com/hcl/capstoneserver/invoice/exceptions/InvoiceNumberExistsException.java
@@ -1,10 +1,10 @@
-package com.hcl.capstoneserver.invoice;
+package com.hcl.capstoneserver.invoice.exceptions;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.web.client.HttpClientErrorException;
 
-public class InvoiceNumberNotExistsException extends HttpClientErrorException {
-    public InvoiceNumberNotExistsException(Integer invoiceNumber) {
+public class InvoiceNumberExistsException extends HttpClientErrorException {
+    public InvoiceNumberExistsException(Integer invoiceNumber) {
         super(HttpStatus.FORBIDDEN, String.format("Invoice number %s is not valid", invoiceNumber));
     }
 }
