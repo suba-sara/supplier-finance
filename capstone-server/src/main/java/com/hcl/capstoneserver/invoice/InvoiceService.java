ae13010c0bb9b6245b6e549ae77c4a53e79ffec8
Shevan Fernando <81354288+shevanfernando@users.noreply.github.com>
2021-05-24 11:53:14 +0530

    develop InvoiceService

--- capstone-server/src/main/java/com/hcl/capstoneserver/invoice/InvoiceService.java
+++ capstone-server/src/main/java/com/hcl/capstoneserver/invoice/InvoiceService.java
@@ -1,34 +1,50 @@
 package com.hcl.capstoneserver.invoice;
 
+import com.hcl.capstoneserver.invoice.dto.InvoiceDTO;
+import com.hcl.capstoneserver.invoice.entities.Invoice;
+import com.hcl.capstoneserver.invoice.repositories.InvoiceRepository;
+import com.hcl.capstoneserver.user.UserService;
+import com.hcl.capstoneserver.user.entities.Client;
+import com.hcl.capstoneserver.user.entities.Supplier;
 import org.modelmapper.ModelMapper;
-import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
+import org.springframework.web.client.HttpClientErrorException;
+
+import java.util.Optional;
 
 @Service
 public class InvoiceService {
 
     private final InvoiceRepository invoiceRepository;
     private final ModelMapper mapper;
+    private final UserService userService;
 
-    public InvoiceService(InvoiceRepository invoiceRepository, ModelMapper mapper) {
+    public InvoiceService(InvoiceRepository invoiceRepository, ModelMapper mapper, UserService userService) {
         this.invoiceRepository = invoiceRepository;
         this.mapper = mapper;
+        this.userService = userService;
     }
 
-    public Invoice createInvoice(InvoiceDTO invoice) {
-//        if (invoiceRepository.findInvoiceByInvoiceNumber(invoice.getInvoiceNumber())) {
-//            throw new InvoiceNumberNotExistsException(invoice.getInvoiceNumber());
+//    public Invoice createInvoice(InvoiceDTO invoice) {
+//
+//        Optional<Client> client = userService.fetchClientById(invoice.getClientId());
+//        Optional<Supplier> supplier = userService.fetchSupplierById(invoice.getSupplierId());
+//
+//        if (!client.isPresent() || !supplier.isPresent()) {
+//            throw new HttpClientErrorException(HttpStatus.NOT_FOUND, "Client or Supplier not found");
 //        }
-
-        return mapper.map(invoiceRepository.save(
-                new Invoice(
-                        invoice.getClientId(),
-                        invoice.getSupplierId(),
-                        invoice.getInvoiceNumber(),
-                        invoice.getInvoiceDate(),
-                        invoice.getAmount(),
-                        invoice.getStatus()
-                )
-        ), Invoice.class);
-    }
+//
+//        return mapper.map(invoiceRepository.save(
+//                new Invoice(
+//                        client.get(),
+//                        supplier.get(),
+//                        invoice.getInvoiceNumber(),
+//                        invoice.getInvoiceDate(),
+//                        invoice.getAmount(),
+//                        invoice.getStatus(),
+//                        invoice.getCurrencyType()
+//                )
+//        ), Invoice.class);
+//    }
 }
