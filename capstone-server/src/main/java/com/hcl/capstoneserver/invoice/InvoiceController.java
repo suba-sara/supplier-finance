7b481294f3a2e53c45c935195f5991ab1a7faee0
Shevan Fernando <81354288+shevanfernando@users.noreply.github.com>
2021-05-25 18:40:44 +0530

    develop invoice controller

--- capstone-server/src/main/java/com/hcl/capstoneserver/invoice/InvoiceController.java
+++ capstone-server/src/main/java/com/hcl/capstoneserver/invoice/InvoiceController.java
@@ -1,7 +1,31 @@
 package com.hcl.capstoneserver.invoice;
 
+import com.hcl.capstoneserver.invoice.dto.InvoiceDTO;
+import com.hcl.capstoneserver.invoice.entities.Invoice;
+import org.modelmapper.ModelMapper;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
+import javax.validation.Valid;
+
+@CrossOrigin
 @RestController
 public class InvoiceController {
+
+    private final InvoiceService invoiceService;
+    private final ModelMapper mapper;
+
+    public InvoiceController(InvoiceService invoiceService, ModelMapper mapper) {
+        this.invoiceService = invoiceService;
+        this.mapper = mapper;
+    }
+
+    @PostMapping("/api/invoices/create")
+    public ResponseEntity<Invoice> createInvoice(@RequestBody InvoiceDTO dto) {
+        return new ResponseEntity<>(invoiceService.createInvoice(dto), HttpStatus.CREATED);
+    }
 }
