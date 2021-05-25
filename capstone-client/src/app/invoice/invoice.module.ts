c7b525d441751493d4a4327f9042c25044c52138
Shashi Srinath <atshashisrinath@gmail.com>
2021-05-25 13:26:57 +0530

    create invoice upload page
    Signed-off-by: Shashi Srinath <atshashisrinath@gmail.com>

--- capstone-client/src/app/invoice/invoice.module.ts
+++ capstone-client/src/app/invoice/invoice.module.ts
@@ -5,15 +5,23 @@ import { SharedModule } from '../shared/shared.module';
 import { ReactiveFormsModule } from '@angular/forms';
 import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatInputModule } from '@angular/material/input';
+import { MatIconModule } from '@angular/material/icon';
+import { MatSelectModule } from '@angular/material/select';
+import { MatButtonModule } from '@angular/material/button';
+import { HttpClientModule } from '@angular/common/http';
 
 @NgModule({
   declarations: [InvoiceUploadComponent],
   imports: [
     CommonModule,
     SharedModule,
+    HttpClientModule,
     ReactiveFormsModule,
     MatFormFieldModule,
     MatInputModule,
+    MatIconModule,
+    MatSelectModule,
+    MatButtonModule,
   ],
 })
 export class InvoiceModule {}
