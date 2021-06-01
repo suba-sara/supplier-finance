04a26eb2f150e006c7e51afe58d1ae6e1dbdef9d
Shashi Srinath <atshashisrinath@gmail.com>
2021-06-01 12:14:50 +0530

    init 403 forbidden page
    Signed-off-by: Shashi Srinath <atshashisrinath@gmail.com>

--- capstone-client/src/app/app.module.ts
+++ capstone-client/src/app/app.module.ts
@@ -5,9 +5,10 @@ import { AppComponent } from './app.component';
 import { CoreModule } from './core/core.module';
 import { CommonModule } from '@angular/common';
 import { SharedModule } from './shared/shared.module';
+import { ForbiddenComponent } from './forbidden/forbidden.component';
 
 @NgModule({
-  declarations: [AppComponent],
+  declarations: [AppComponent, ForbiddenComponent],
   imports: [CommonModule, BrowserModule, CoreModule, SharedModule],
   providers: [],
   bootstrap: [AppComponent],
