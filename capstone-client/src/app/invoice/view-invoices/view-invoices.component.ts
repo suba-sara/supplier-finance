47fdda8fcd1f24217496c533addd88e1b76668e4
Shashi Srinath <atshashisrinath@gmail.com>
2021-06-01 17:35:21 +0530

    add fake invoice data array
    Signed-off-by: Shashi Srinath <atshashisrinath@gmail.com>

--- capstone-client/src/app/invoice/view-invoices/view-invoices.component.ts
+++ capstone-client/src/app/invoice/view-invoices/view-invoices.component.ts
@@ -1,4 +1,48 @@
 import { Component, OnInit } from '@angular/core';
+import { Invoice } from '../invoice.types';
+
+const fakeData: Invoice[] = [
+  {
+    invoiceId: 13,
+    invoiceDate: new Date(),
+    invoiceNumber: 12131,
+    supplierId: 'aasd11',
+    clientId: 'wfass',
+    amount: 200,
+    currencyType: 'EUR',
+    status: 'pending',
+  },
+  {
+    invoiceId: 14,
+    invoiceDate: new Date(),
+    invoiceNumber: 1222131,
+    supplierId: 'aaaasd11',
+    clientId: 'wferass',
+    amount: 200,
+    currencyType: 'EUR',
+    status: 'pending',
+  },
+  {
+    invoiceId: 15,
+    invoiceDate: new Date(),
+    invoiceNumber: 12112431,
+    supplierId: 'aasd11',
+    clientId: 'wfass',
+    amount: 200,
+    currencyType: 'EUR',
+    status: 'pending',
+  },
+  {
+    invoiceId: 16,
+    invoiceDate: new Date(),
+    invoiceNumber: 12325131,
+    supplierId: 'aasd11',
+    clientId: 'wfass',
+    amount: 200,
+    currencyType: 'EUR',
+    status: 'pending',
+  },
+];
 
 @Component({
   selector: 'app-view-invoices',
