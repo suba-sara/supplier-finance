6a072c048ca45cf0eb7b0491b78e2ddfa0e35243
Shashi Srinath <atshashisrinath@gmail.com>
2021-06-02 13:25:17 +0530

    create functions to format date and get age
    Signed-off-by: Shashi Srinath <atshashisrinath@gmail.com>

--- capstone-client/src/app/invoice/view-invoices/view-invoices.component.ts
+++ capstone-client/src/app/invoice/view-invoices/view-invoices.component.ts
@@ -1,10 +1,11 @@
 import { Component, OnInit } from '@angular/core';
 import { Invoice } from '../invoice.types';
+import * as dayjs from 'dayjs';
 
 const fakeData: Invoice[] = [
   {
     invoiceId: 13,
-    invoiceDate: new Date(),
+    invoiceDate: new Date('2021-11-23'),
     invoiceNumber: 12131,
     supplierId: 'aasd11',
     clientId: 'wfass',
@@ -14,7 +15,7 @@ const fakeData: Invoice[] = [
   },
   {
     invoiceId: 14,
-    invoiceDate: new Date(),
+    invoiceDate: new Date('2021-07-23'),
     invoiceNumber: 1222131,
     supplierId: 'aaaasd11',
     clientId: 'wferass',
@@ -24,7 +25,7 @@ const fakeData: Invoice[] = [
   },
   {
     invoiceId: 15,
-    invoiceDate: new Date(),
+    invoiceDate: new Date('2021-06-11'),
     invoiceNumber: 12112431,
     supplierId: 'aasd11',
     clientId: 'wfass',
@@ -34,7 +35,17 @@ const fakeData: Invoice[] = [
   },
   {
     invoiceId: 16,
-    invoiceDate: new Date(),
+    invoiceDate: new Date('2023-05-30'),
+    invoiceNumber: 12325131,
+    supplierId: 'aasd11',
+    clientId: 'wfass',
+    amount: 200,
+    currencyType: 'EUR',
+    status: 'pending',
+  },
+  {
+    invoiceId: 121,
+    invoiceDate: new Date('2022-01-30'),
     invoiceNumber: 12325131,
     supplierId: 'aasd11',
     clientId: 'wfass',
@@ -51,9 +62,40 @@ const fakeData: Invoice[] = [
 })
 export class ViewInvoicesComponent implements OnInit {
   dataSource = fakeData;
-  displayedColumns: string[] = ['invoiceId', 'invoiceNumber', 'amount'];
+  displayedColumns: string[] = [
+    'supplierId',
+    'invoiceDate',
+    'amount',
+    'invoiceNumber',
+    'invoiceAge',
+    'invoiceId',
+  ];
 
   constructor() {}
 
   ngOnInit(): void {}
+
+  formatDate(date: Date): string {
+    return dayjs(date).format('DD-MMM-YYYY');
+  }
+
+  invoiceAge(date: Date): string {
+    const formatOutput = (value: number, unit: string) =>
+      value === 1 ? `${value} ${unit}` : `${value} ${unit}s`;
+
+    const today = dayjs();
+
+    const diffYears = dayjs(date).diff(today, 'years');
+    if (diffYears) {
+      return formatOutput(diffYears, 'year');
+    }
+
+    const diffMonths = dayjs(date).diff(today, 'months');
+    if (diffMonths) {
+      return formatOutput(diffMonths, 'month');
+    }
+
+    const diffDays = dayjs(date).diff(today, 'days');
+    return formatOutput(diffDays, 'day');
+  }
 }
