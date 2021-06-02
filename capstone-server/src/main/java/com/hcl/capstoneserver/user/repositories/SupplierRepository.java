de34dd0de80a65ab2555373c2de3e544854931b3
Shevan Fernando <81354288+shevanfernando@users.noreply.github.com>
2021-06-02 22:59:15 +0530

    code format

--- capstone-server/src/main/java/com/hcl/capstoneserver/user/repositories/SupplierRepository.java
+++ capstone-server/src/main/java/com/hcl/capstoneserver/user/repositories/SupplierRepository.java
@@ -2,10 +2,6 @@ package com.hcl.capstoneserver.user.repositories;
 
 import com.hcl.capstoneserver.user.entities.Supplier;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Query;
 
 public interface SupplierRepository extends JpaRepository<Supplier, String> {
-
-    @Query("SELECT CASE WHEN COUNT(s)>0 THEN true ELSE false END FROM Supplier AS s WHERE s.email = ?1")
-    Boolean existsByEmail(String email);
 }
