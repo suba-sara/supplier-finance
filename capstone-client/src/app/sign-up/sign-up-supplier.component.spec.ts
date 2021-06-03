59368946e2172618349bdc6045d01dca9124d99f
Hashen Weerapperuma <hashen.weerapperuma@gmail.com>
2021-06-03 10:40:19 +0530

    fixing
    Signed-off-by: Hashen Weerapperuma <hashen.weerapperuma@gmail.com>

--- capstone-client/src/app/sign-up/sign-up-supplier.component.spec.ts
+++ capstone-client/src/app/sign-up/sign-up-supplier.component.spec.ts
@@ -1,17 +1,18 @@
-import { ComponentFixture, TestBed } from '@angular/core/testing';
+import {ComponentFixture, TestBed} from '@angular/core/testing';
 
-import { SignUpClientComponent } from './sign-up-client.component';
-import { CommonModule } from '@angular/common';
-import { RouterTestingModule } from '@angular/router/testing';
-import { MatInputModule } from '@angular/material/input';
-import { MatButtonModule } from '@angular/material/button';
-import { FormsModule, ReactiveFormsModule } from '@angular/forms';
-import { SignUpComponent } from './sign-up.component';
-import { SignUpSupplierComponent } from './sign-up-supplier.component';
-import { PersonalDataFormComponent } from './personal-data-form/personal-data-form.component';
-import { BankAccountDetailsFormComponent } from './bank-account-details-form/bank-account-details-form.component';
-import { UserDataFormComponent } from './user-data-form/user-data-form.component';
-import { UserTypeButtonComponent } from './user-type-button/user-type-button.component';
+import {SignUpClientComponent} from './sign-up-client.component';
+import {CommonModule} from '@angular/common';
+import {RouterTestingModule} from '@angular/router/testing';
+import {MatInputModule} from '@angular/material/input';
+import {MatButtonModule} from '@angular/material/button';
+import {FormsModule, ReactiveFormsModule} from '@angular/forms';
+import {SignUpComponent} from './sign-up.component';
+import {SignUpSupplierComponent} from './sign-up-supplier.component';
+import {PersonalDataFormComponent} from './personal-data-form/personal-data-form.component';
+import {BankAccountDetailsFormComponent} from './bank-account-details-form/bank-account-details-form.component';
+import {UserDataFormComponent} from './user-data-form/user-data-form.component';
+import {UserTypeButtonComponent} from './user-type-button/user-type-button.component';
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
 
 describe('SignUpComponent', () => {
   let component: SignUpClientComponent;
@@ -26,6 +27,7 @@ describe('SignUpComponent', () => {
         MatButtonModule,
         FormsModule,
         ReactiveFormsModule,
+        BrowserAnimationsModule
       ],
       declarations: [
         SignUpComponent,
