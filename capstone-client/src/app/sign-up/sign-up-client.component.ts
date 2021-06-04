3918fc2f32e662caccb0d6bc63a48fb820701f9f
Shashi Srinath <atshashisrinath@gmail.com>
2021-06-04 11:35:34 +0530

    connect form with api
    Signed-off-by: Shashi Srinath <atshashisrinath@gmail.com>

--- capstone-client/src/app/sign-up/sign-up-client.component.ts
+++ capstone-client/src/app/sign-up/sign-up-client.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { UserDetails } from './user-data-form/user-data-form.component';
 import { PersonalDetails } from './personal-data-form/personal-data-form.component';
+import { SignUpService } from './sign-up.service';
 
 @Component({
   selector: 'app-sign-up-client',
@@ -8,12 +9,29 @@ import { PersonalDetails } from './personal-data-form/personal-data-form.compone
   styleUrls: ['./sign-up-client.component.scss'],
 })
 export class SignUpClientComponent implements OnInit {
-  personalDetails?: PersonalDetails;
-  userDetails?: UserDetails;
+  personalDetails: PersonalDetails;
+  userDetails: UserDetails;
   pageNumber: number;
 
-  constructor() {
+  constructor(private signUpService: SignUpService) {
     this.pageNumber = 1;
+
+    this.personalDetails = {
+      firstName: '',
+      lastName: '',
+      email: '',
+      phone: '',
+      addressLine1: '',
+      city: '',
+      state: '',
+      province: '',
+      country: '',
+    };
+
+    this.pageNumber = 1;
+
+    // this.clientAccDetails = {};
+    this.userDetails = { password: '', userId: '' };
   }
 
   submitUserData = (value: UserDetails): void => {
@@ -37,5 +55,19 @@ export class SignUpClientComponent implements OnInit {
     if (this.pageNumber < 3) {
       this.pageNumber = this.pageNumber + 1;
     }
+  };
+
+  register = (): void => {
+    this.signUpService
+      .signUpClient({
+        ...this.userDetails,
+        ...this.personalDetails,
+      })
+      .subscribe(
+        () => {
+          this.pageNumber = 4;
+        },
+        (error) => alert(error)
+      );
   };
 }
