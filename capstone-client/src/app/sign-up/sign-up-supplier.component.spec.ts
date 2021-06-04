d19db31c5e43a5ea8460f907e6117177c516245f
Shashi Srinath <atshashisrinath@gmail.com>
2021-06-04 11:50:16 +0530

    fix the issue with tests
    Signed-off-by: Shashi Srinath <atshashisrinath@gmail.com>

--- capstone-client/src/app/sign-up/sign-up-supplier.component.spec.ts
+++ capstone-client/src/app/sign-up/sign-up-supplier.component.spec.ts
@@ -1,18 +1,19 @@
-import {ComponentFixture, TestBed} from '@angular/core/testing';
+import { ComponentFixture, TestBed } from '@angular/core/testing';
 
-import {SignUpClientComponent} from './sign-up-client.component';
-import {CommonModule} from '@angular/common';
-import {RouterTestingModule} from '@angular/router/testing';
-import {MatInputModule} from '@angular/material/input';
-import {MatButtonModule} from '@angular/material/button';
-import {FormsModule, ReactiveFormsModule} from '@angular/forms';
-import {SignUpComponent} from './sign-up.component';
-import {SignUpSupplierComponent} from './sign-up-supplier.component';
-import {PersonalDataFormComponent} from './personal-data-form/personal-data-form.component';
-import {BankAccountDetailsFormComponent} from './bank-account-details-form/bank-account-details-form.component';
-import {UserDataFormComponent} from './user-data-form/user-data-form.component';
-import {UserTypeButtonComponent} from './user-type-button/user-type-button.component';
-import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
+import { SignUpClientComponent } from './sign-up-client.component';
+import { CommonModule } from '@angular/common';
+import { RouterTestingModule } from '@angular/router/testing';
+import { MatInputModule } from '@angular/material/input';
+import { MatButtonModule } from '@angular/material/button';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { SignUpComponent } from './sign-up.component';
+import { SignUpSupplierComponent } from './sign-up-supplier.component';
+import { PersonalDataFormComponent } from './personal-data-form/personal-data-form.component';
+import { BankAccountDetailsFormComponent } from './bank-account-details-form/bank-account-details-form.component';
+import { UserDataFormComponent } from './user-data-form/user-data-form.component';
+import { UserTypeButtonComponent } from './user-type-button/user-type-button.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { HttpClientTestingModule } from '@angular/common/http/testing';
 
 describe('SignUpComponent', () => {
   let component: SignUpClientComponent;
@@ -23,11 +24,12 @@ describe('SignUpComponent', () => {
       imports: [
         CommonModule,
         RouterTestingModule,
+        HttpClientTestingModule,
         MatInputModule,
         MatButtonModule,
         FormsModule,
         ReactiveFormsModule,
-        BrowserAnimationsModule
+        BrowserAnimationsModule,
       ],
       declarations: [
         SignUpComponent,
