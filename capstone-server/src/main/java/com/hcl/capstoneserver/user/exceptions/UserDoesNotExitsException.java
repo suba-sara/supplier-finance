83639593426b371740b2d2d5e013e562d0d6e602
Shevan Fernando <81354288+shevanfernando@users.noreply.github.com>
2021-06-06 13:40:52 +0530

    rename this exception class as a UserDoesNotExistException

--- capstone-server/src/main/java/com/hcl/capstoneserver/user/exceptions/UserDoesNotExitsException.java
+++
@@ -1,9 +0,0 @@
-package com.hcl.capstoneserver.user.exceptions;
-
-import com.hcl.capstoneserver.user.UserType;
-
-public class UserDoesNotExitsException extends RuntimeException {
-    public UserDoesNotExitsException(String username, UserType userType) {
-        super(String.format("%s with username %s not exits!", userType, username));
-    }
-}
