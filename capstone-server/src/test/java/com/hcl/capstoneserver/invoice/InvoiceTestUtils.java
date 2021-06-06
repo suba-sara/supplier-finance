6065b6ea0796d513e750bc4aec9314af85faa436
Shevan Fernando <81354288+shevanfernando@users.noreply.github.com>
2021-06-06 13:22:55 +0530

    add new method for create expiredInvoice

--- capstone-server/src/test/java/com/hcl/capstoneserver/invoice/InvoiceTestUtils.java
+++ capstone-server/src/test/java/com/hcl/capstoneserver/invoice/InvoiceTestUtils.java
@@ -3,6 +3,11 @@ package com.hcl.capstoneserver.invoice;
 import com.hcl.capstoneserver.invoice.dto.ClientViewInvoiceDTO;
 import com.hcl.capstoneserver.invoice.dto.CreateInvoiceDTO;
 import com.hcl.capstoneserver.invoice.entities.Invoice;
+import com.hcl.capstoneserver.invoice.repositories.InvoiceRepository;
+import com.hcl.capstoneserver.user.UserService;
+import com.hcl.capstoneserver.user.dto.ClientDTO;
+import com.hcl.capstoneserver.user.dto.SupplierDTO;
+import com.hcl.capstoneserver.user.repositories.ClientRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
@@ -15,22 +20,43 @@ public class InvoiceTestUtils {
     @Autowired
     InvoiceService invoiceService;
 
-    public List<ClientViewInvoiceDTO> createInvoice() {
+    @Autowired
+    InvoiceRepository invoiceRepository;
+
+    @Autowired
+    UserService userService;
+
+    @Autowired
+    ClientRepository clientRepository;
+
+    public List<ClientViewInvoiceDTO> createInvoice(List<SupplierDTO> supplier) {
         List<ClientViewInvoiceDTO> dtoList = new ArrayList<>();
         dtoList.add(invoiceService.createInvoice(new CreateInvoiceDTO(
-                "SP_00001",
+                supplier.get(0).getSupplierId(),
                 "1234567898",
                 LocalDate.now(),
                 25000.0,
                 CurrencyType.USD
         ), "client"));
         dtoList.add(invoiceService.createInvoice(new CreateInvoiceDTO(
-                "SP_00002",
+                supplier.get(1).getSupplierId(),
                 "1234567899",
                 LocalDate.now(),
                 25000.0,
                 CurrencyType.USD
         ), "client2"));
         return dtoList;
+    }
+
+    public Invoice createExpiredInvoice(List<SupplierDTO> supplier, List<ClientDTO> client) {
+        return invoiceRepository.save(new Invoice(
+                userService.fetchClientDataByClientId(client.get(0).getClientId()),
+                userService.fetchSupplierDataBySupplierId(supplier.get(0).getSupplierId()),
+                "999999999",
+                LocalDate.parse("2021-05-01"),
+                40000.0,
+                InvoiceStatus.UPLOADED,
+                CurrencyType.GBP
+        ));
     }
 }
