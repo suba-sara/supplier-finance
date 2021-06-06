ec6b9f90f1b0e65f55490a7ed6286fac1dd23504
Shevan Fernando <81354288+shevanfernando@users.noreply.github.com>
2021-06-06 13:48:40 +0530

    update global error handler

--- capstone-server/src/main/java/com/hcl/capstoneserver/config/GlobalErrorHandler.java
+++ capstone-server/src/main/java/com/hcl/capstoneserver/config/GlobalErrorHandler.java
@@ -2,8 +2,10 @@ package com.hcl.capstoneserver.config;
 
 import com.hcl.capstoneserver.config.error_responses.DefaultErrorResponse;
 import com.hcl.capstoneserver.config.error_responses.DefaultValidationErrorResponse;
+import com.hcl.capstoneserver.invoice.exception.*;
 import com.hcl.capstoneserver.user.exceptions.EmailAlreadyExistsException;
 import com.hcl.capstoneserver.user.exceptions.UserAlreadyExistsException;
+import com.hcl.capstoneserver.user.exceptions.UserDoesNotExistException;
 import io.jsonwebtoken.JwtException;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
@@ -20,10 +22,19 @@ import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExcep
 
 import java.util.ArrayList;
 import java.util.LinkedHashMap;
+import java.util.List;
 import java.util.Map;
 
 @ControllerAdvice
 public class GlobalErrorHandler extends ResponseEntityExceptionHandler {
+
+    private Map<String, String> _getErrorsMaps(String field, String message) {
+        Map<String, String> error = new LinkedHashMap<>();
+        error.put("field", field);
+        error.put("message", message);
+        return error;
+    }
+
     @Override
     protected ResponseEntity<Object> handleMethodArgumentNotValid(
             MethodArgumentNotValidException ex,
@@ -33,11 +44,7 @@ public class GlobalErrorHandler extends ResponseEntityExceptionHandler {
         ArrayList<Map<String, String>> errors = new ArrayList<>();
 
         for (FieldError fieldError : ex.getBindingResult().getFieldErrors()) {
-            LinkedHashMap<String, String> error = new LinkedHashMap<>();
-            error.put("field", fieldError.getField());
-            error.put("message", fieldError.getDefaultMessage());
-
-            errors.add(error);
+            errors.add(_getErrorsMaps(fieldError.getField(), fieldError.getDefaultMessage()));
         }
 
         return new ResponseEntity<>(
@@ -93,15 +100,12 @@ public class GlobalErrorHandler extends ResponseEntityExceptionHandler {
     @ExceptionHandler(EmailAlreadyExistsException.class)
     public final ResponseEntity<Object> handleEmailExistsException(EmailAlreadyExistsException ex) {
         ArrayList<Map<String, String>> errors = new ArrayList<>();
-        Map<String, String> error = new LinkedHashMap<>();
-        error.put("field", "email");
-        error.put("message", ex.getMessage());
-        errors.add(error);
+        errors.add(_getErrorsMaps("Email", ex.getMessage()));
 
         return new ResponseEntity<>(
                 new DefaultValidationErrorResponse(
                         HttpStatus.BAD_REQUEST,
-                        "An account with this email address already exists. please sign in or use another email",
+                        "Email Already Exists.",
                         errors
                 ),
                 HttpStatus.BAD_REQUEST
@@ -111,15 +115,84 @@ public class GlobalErrorHandler extends ResponseEntityExceptionHandler {
     @ExceptionHandler(UserAlreadyExistsException.class)
     protected final ResponseEntity<Object> handleUserIdExistsException(UserAlreadyExistsException ex) {
         ArrayList<Map<String, String>> errors = new ArrayList<>();
-        Map<String, String> error = new LinkedHashMap<>();
-        error.put("field", "userId");
-        error.put("message", ex.getMessage());
-        errors.add(error);
+        errors.add(_getErrorsMaps("User Id", ex.getMessage()));
 
         return new ResponseEntity<>(
-                new DefaultValidationErrorResponse(HttpStatus.BAD_REQUEST, "User Already Exists", errors),
+                new DefaultValidationErrorResponse(HttpStatus.BAD_REQUEST, "User Already Exists.", errors),
                 HttpStatus.BAD_REQUEST
         );
+    }
+
+    @ExceptionHandler(UserDoesNotExistException.class)
+    protected final ResponseEntity<Object> handleUserNotFoundException(UserDoesNotExistException ex) {
+        List<Map<String, String>> errors = new ArrayList<>();
+        errors.add(_getErrorsMaps(ex.getField(), ex.getMessage()));
+
+        return new ResponseEntity<>(
+                new DefaultValidationErrorResponse(HttpStatus.BAD_REQUEST, "User Not Found.", errors),
+                HttpStatus.BAD_REQUEST
+        );
+    }
+
+    @ExceptionHandler(InvoiceNumberAlreadyExistsSupplierException.class)
+    protected final ResponseEntity<Object> handleInvoiceNumberAlreadyExistsSupplierException(
+            InvoiceNumberAlreadyExistsSupplierException ex
+    ) {
+        List<Map<String, String>> errors = new ArrayList<>();
+        errors.add(_getErrorsMaps(ex.getField(), ex.getMessage()));
+
+        return new ResponseEntity<>(
+                new DefaultValidationErrorResponse(
+                        HttpStatus.BAD_REQUEST,
+                        "Invoice Number Is Exists For Supplier.",
+                        errors
+                ),
+                HttpStatus.BAD_REQUEST
+        );
+    }
+
+    @ExceptionHandler(InvoiceDateOldException.class)
+    protected final ResponseEntity<Object> handleInvoiceDateOldException(InvoiceDateOldException ex) {
+        List<Map<String, String>> errors = new ArrayList<>();
+        errors.add(_getErrorsMaps(ex.getField(), ex.getMessage()));
+        return new ResponseEntity<>(new DefaultValidationErrorResponse(
+                HttpStatus.BAD_REQUEST,
+                "Invoice Date Not A Future Date.",
+                errors
+        ), HttpStatus.BAD_REQUEST);
+    }
+
+    @ExceptionHandler(InvoiceOwnershipException.class)
+    protected final ResponseEntity<Object> handleInvoiceOwnershipException(InvoiceOwnershipException ex) {
+        List<Map<String, String>> errors = new ArrayList<>();
+        errors.add(_getErrorsMaps(ex.getField(), ex.getMessage()));
+        return new ResponseEntity<>(new DefaultValidationErrorResponse(
+                HttpStatus.BAD_REQUEST,
+                "Invoice Access Denied.",
+                errors
+        ), HttpStatus.BAD_REQUEST);
+    }
+
+    @ExceptionHandler(InvoiceNotFoundException.class)
+    protected final ResponseEntity<Object> handleInvoiceNotFoundException(InvoiceNotFoundException ex) {
+        List<Map<String, String>> errors = new ArrayList<>();
+        errors.add(_getErrorsMaps(ex.getField(), ex.getMessage()));
+        return new ResponseEntity<>(new DefaultValidationErrorResponse(
+                HttpStatus.BAD_REQUEST,
+                "Invoice Not Found.",
+                errors
+        ), HttpStatus.BAD_REQUEST);
+    }
+
+    @ExceptionHandler(InvoiceStatusException.class)
+    protected final ResponseEntity<Object> handleInvoiceStatusException(InvoiceStatusException ex) {
+        List<Map<String, String>> errors = new ArrayList<>();
+        errors.add(_getErrorsMaps(ex.getField(), ex.getMessage()));
+        return new ResponseEntity<>(new DefaultValidationErrorResponse(
+                HttpStatus.BAD_REQUEST,
+                "Invoice Status Validate.",
+                errors
+        ), HttpStatus.BAD_REQUEST);
     }
 }
 
