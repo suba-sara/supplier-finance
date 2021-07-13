2f6e795197eeda5fa084833de7299f42e233090a
Shevan Fernando <81354288+shevanfernando@users.noreply.github.com>
2021-07-13 09:43:51 +0530

    add view single invoice component

--- capstone-client/src/app/invoice/invoice.module.ts
+++ capstone-client/src/app/invoice/invoice.module.ts
@@ -13,9 +13,14 @@ import { ViewInvoicesComponent } from './view-invoices/view-invoices.component';
 import { MatTableModule } from '@angular/material/table';
 import { MatTooltipModule } from '@angular/material/tooltip';
 import { MatPaginatorModule } from '@angular/material/paginator';
+import { ViewSingleInvoiceComponent } from './view-single-invoice/view-single-invoice.component';
 
 @NgModule({
-  declarations: [InvoiceUploadComponent, ViewInvoicesComponent],
+  declarations: [
+    InvoiceUploadComponent,
+    ViewInvoicesComponent,
+    ViewSingleInvoiceComponent,
+  ],
   imports: [
     CommonModule,
     SharedModule,
