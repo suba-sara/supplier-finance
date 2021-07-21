1268f9aa7911fbf7fc8fbeef58434cf051a0a113
Shashi Srinath <atshashisrinath@gmail.com>
2021-07-21 14:08:00 +0530

    create ViewInvoiceDTO.java
    Signed-off-by: Shashi Srinath <atshashisrinath@gmail.com>
    
    Took 6 minutes

---
+++ capstone-server/src/main/java/com/hcl/capstoneserver/invoice/dto/ViewInvoiceDTO.java
@@ -0,0 +1,131 @@
+package com.hcl.capstoneserver.invoice.dto;
+
+import com.hcl.capstoneserver.invoice.CurrencyType;
+import com.hcl.capstoneserver.invoice.InvoiceStatus;
+import com.hcl.capstoneserver.user.entities.Client;
+import com.hcl.capstoneserver.user.entities.Supplier;
+
+import java.time.LocalDate;
+
+public class ViewInvoiceDTO {
+    private final String fileUriBase = "/api/invoice/file/";
+
+    private Integer invoiceId;
+    private Client client;
+    private Supplier supplier;
+    private String file;
+    private String invoiceNumber;
+    private LocalDate uploadedDate;
+    private LocalDate invoiceDate;
+    private Double amount;
+    private InvoiceStatus status;
+    private CurrencyType currencyType;
+
+    public ViewInvoiceDTO(
+            Integer invoiceId,
+            Client client,
+            Supplier supplier,
+            String invoiceNumber,
+            LocalDate uploadedDate,
+            LocalDate invoiceDate,
+            Double amount,
+            InvoiceStatus status,
+            CurrencyType currencyType
+    ) {
+        this.invoiceId = invoiceId;
+        this.client = client;
+        this.supplier = supplier;
+        this.invoiceNumber = invoiceNumber;
+        this.uploadedDate = uploadedDate;
+        this.invoiceDate = invoiceDate;
+        this.amount = amount;
+        this.status = status;
+        this.currencyType = currencyType;
+
+        this.file = fileUriBase + invoiceId;
+    }
+
+    public String getFileUriBase() {
+        return fileUriBase;
+    }
+
+    public Integer getInvoiceId() {
+        return invoiceId;
+    }
+
+    public void setInvoiceId(Integer invoiceId) {
+        this.invoiceId = invoiceId;
+    }
+
+    public Client getClient() {
+        return client;
+    }
+
+    public void setClient(Client client) {
+        this.client = client;
+    }
+
+    public Supplier getSupplier() {
+        return supplier;
+    }
+
+    public void setSupplier(Supplier supplier) {
+        this.supplier = supplier;
+    }
+
+    public String getFile() {
+        return file;
+    }
+
+    public void setFile(String file) {
+        this.file = file;
+    }
+
+    public String getInvoiceNumber() {
+        return invoiceNumber;
+    }
+
+    public void setInvoiceNumber(String invoiceNumber) {
+        this.invoiceNumber = invoiceNumber;
+    }
+
+    public LocalDate getUploadedDate() {
+        return uploadedDate;
+    }
+
+    public void setUploadedDate(LocalDate uploadedDate) {
+        this.uploadedDate = uploadedDate;
+    }
+
+    public LocalDate getInvoiceDate() {
+        return invoiceDate;
+    }
+
+    public void setInvoiceDate(LocalDate invoiceDate) {
+        this.invoiceDate = invoiceDate;
+    }
+
+    public Double getAmount() {
+        return amount;
+    }
+
+    public void setAmount(Double amount) {
+        this.amount = amount;
+    }
+
+    public InvoiceStatus getStatus() {
+        return status;
+    }
+
+    public void setStatus(InvoiceStatus status) {
+        this.status = status;
+    }
+
+    public CurrencyType getCurrencyType() {
+        return currencyType;
+    }
+
+    public void setCurrencyType(CurrencyType currencyType) {
+        this.currencyType = currencyType;
+    }
+}
