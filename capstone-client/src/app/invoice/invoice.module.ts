c693c137efff7e3e14dfd8bea74d852c3d045093
Shevan Fernando <81354288+shevanfernando@users.noreply.github.com>
2021-07-22 08:56:51 +0530

    import reactive form module

--- capstone-client/src/app/invoice/invoice.module.ts
+++ capstone-client/src/app/invoice/invoice.module.ts
@@ -2,7 +2,7 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { InvoiceUploadComponent } from './invoice-upload/invoice-upload.component';
 import { SharedModule } from '../shared/shared.module';
-import { ReactiveFormsModule } from '@angular/forms';
+import {FormsModule, ReactiveFormsModule} from '@angular/forms';
 import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatInputModule } from '@angular/material/input';
 import { MatIconModule } from '@angular/material/icon';
@@ -28,20 +28,21 @@ import { MatSortModule } from '@angular/material/sort';
     DateFormatPipe,
     InvoiceAgePipe,
   ],
-  imports: [
-    CommonModule,
-    SharedModule,
-    HttpClientModule,
-    ReactiveFormsModule,
-    MatFormFieldModule,
-    MatInputModule,
-    MatIconModule,
-    MatSelectModule,
-    MatButtonModule,
-    MatTableModule,
-    MatTooltipModule,
-    MatPaginatorModule,
-    MatSortModule,
-  ],
+    imports: [
+        CommonModule,
+        SharedModule,
+        HttpClientModule,
+        ReactiveFormsModule,
+        MatFormFieldModule,
+        MatInputModule,
+        MatIconModule,
+        MatSelectModule,
+        MatButtonModule,
+        MatTableModule,
+        MatTooltipModule,
+        MatPaginatorModule,
+        MatSortModule,
+        FormsModule,
+    ],
 })
 export class InvoiceModule {}
