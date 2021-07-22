ecf9d7724ecac91e76c85ee1c3adad577f632cfb
Shevan Fernando <81354288+shevanfernando@users.noreply.github.com>
2021-07-22 11:00:08 +0530

    view-single-invoice.service.ts update

--- capstone-client/src/app/invoice/view-single-invoice/view-single-invoice.service.ts
+++ capstone-client/src/app/invoice/view-single-invoice/view-single-invoice.service.ts
@@ -1,20 +1,34 @@
-import { Injectable } from '@angular/core';
-import { HttpClient } from '@angular/common/http';
-import { environment } from '../../../environments/environment';
-import { Invoice } from '../invoice.types';
+import {Injectable} from '@angular/core';
+import {HttpClient} from '@angular/common/http';
+import {environment} from '../../../environments/environment';
+import {Invoice} from '../invoice.types';
 
-const { API_PATH } = environment;
+const {API_PATH} = environment;
 
 @Injectable({
   providedIn: 'root',
 })
 export class ViewSingleInvoicesService {
-  constructor(private http: HttpClient) {}
+  constructor(private http: HttpClient) {
+  }
 
   public getInvoiceById(invoiceId: number): Promise<Invoice> {
     console.log(invoiceId);
     return this.http
       .get<Invoice>(`${API_PATH}/invoices/getById/${invoiceId}`)
       .toPromise();
+  }
+
+  public editInvoice(data: any, userType: string): Promise<any> | null {
+    console.log(data);
+    switch (userType) {
+      case 'BANK': {
+        return this.http.put<any>(`${API_PATH}/invoices/retrieve/bank`, data).toPromise();
+      }
+      case 'CLIENT': {
+        return this.http.put<any>(`${API_PATH}/invoices/retrieve/bank`, data).toPromise();
+      }
+    }
+    return null;
   }
 }
