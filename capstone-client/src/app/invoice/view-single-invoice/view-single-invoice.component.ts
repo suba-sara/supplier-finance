f8f42e10a5302c4e6464063aa3894a5054bd3cf3
Shevan Fernando <81354288+shevanfernando@users.noreply.github.com>
2021-07-22 11:00:26 +0530

    view-single-invoice.component.ts update

--- capstone-client/src/app/invoice/view-single-invoice/view-single-invoice.component.ts
+++ capstone-client/src/app/invoice/view-single-invoice/view-single-invoice.component.ts
@@ -3,9 +3,10 @@ import {ActivatedRoute, Router} from '@angular/router';
 import {ViewSingleInvoicesService} from './view-single-invoice.service';
 import {Invoice} from '../invoice.types';
 import {environment} from '../../../environments/environment';
-import {FormBuilder, Validators} from '@angular/forms';
+import {FormBuilder, FormControl, Validators} from '@angular/forms';
 import {InvoiceUploadService} from '../invoice-upload/invoice-upload.service';
 import {SnackbarService} from '../../util/snakbar.service';
+import {AuthService} from '../../core/auth/auth.service';
 
 @Component({
   selector: 'app-view-single-invoice',
@@ -14,8 +15,26 @@ import {SnackbarService} from '../../util/snakbar.service';
 })
 export class ViewSingleInvoiceComponent implements OnInit {
   SERVER = environment.SERVER;
-  invoice?: Invoice;
+  invoice?: Invoice = {
+    invoiceId: 0,
+    client: {
+      clientId: '',
+      name: '',
+    },
+    supplier: {
+      supplierId: '',
+      name: '',
+    },
+    invoiceNumber: 0,
+    uploadedDate: '',
+    invoiceDate: '',
+    amount: 0,
+    status: '',
+    currencyType: 'USD',
+    fileUrl: ''
+  };
   edit: boolean;
+  userTypeApiPath = 'SUPPLIER';
 
   constructor(
     private _router: Router,
@@ -23,18 +42,23 @@ export class ViewSingleInvoiceComponent implements OnInit {
     private viewSingleInvoicesService: ViewSingleInvoicesService,
     private fb: FormBuilder,
     private invoiceUploadService: InvoiceUploadService,
-    private snackBarService: SnackbarService
+    private snackBarService: SnackbarService,
+    private authService: AuthService
   ) {
     const invoiceId = this.route.snapshot.params['id'];
     this.edit = this.route.snapshot.params['edit'] === 'edit';
     this.viewSingleInvoicesService
       .getInvoiceById(invoiceId)
       .then((invoice) => (this.invoice = invoice));
+    authService.user$.subscribe((res: any) => {
+      this.userTypeApiPath = res.userType;
+    });
   }
 
   invoiceForm = this.fb.group({
     status: ['', Validators.required],
-    amount: ['', [Validators.required, Validators.min(0), Validators.pattern('\\^([\\\\d]{0,4})(\\\\.|$)([\\\\d]{2,2}|)$')]]
+    amount: ['', [Validators.required, Validators.min(0), Validators.pattern('\\^([\\\\d]{0,4})(\\\\.|$)([\\\\d]{2,2}|)$')]],
+    currencyType: new FormControl(this.invoice?.currencyType, {validators: [Validators.required]}),
   });
 
   ngOnInit(): void {
@@ -43,23 +67,19 @@ export class ViewSingleInvoiceComponent implements OnInit {
   async onClickSave(): Promise<any> {
     this.invoiceForm.markAllAsTouched();
     console.log(this.invoiceForm.get('invoiceFile')?.value);
-
     if (!this.invoiceForm.errors) {
-      await this.invoiceUploadService
-        .createInvoice(this.invoiceForm.value)
-        .then((res) => {
-          this.snackBarService.snackBar?.open(
-            'Invoice Update Successfully',
-            undefined,
-            {
-              duration: 2000,
-            }
-          );
-          this._router.navigate([
-            `invoice/view-invoice/${res.invoice.invoiceId}/view`,
-          ]);
-        })
-        .catch((e) => console.log(e));
+      await this.viewSingleInvoicesService.editInvoice(this.invoiceForm.value, this.userTypeApiPath)?.then((res) => {
+        this.snackBarService.snackBar?.open(
+          'Invoice Update Successfully',
+          undefined,
+          {
+            duration: 2000,
+          }
+        );
+        this._router.navigate([
+          `invoice/view-invoice/${res.invoice.invoiceId}/view`,
+        ]);
+      }).catch(err => console.error(err));
     }
   }
 }
