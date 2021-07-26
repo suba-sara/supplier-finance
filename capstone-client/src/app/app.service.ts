edcaf9cf267965fc6354fdcf21310946b1850f4f
Shashi Srinath <atshashisrinath@gmail.com>
2021-07-26 13:37:14 +0530

    store leftnavbar state on localstorage
    Signed-off-by: Shashi Srinath <atshashisrinath@gmail.com>
    
    Took 10 minutes

--- capstone-client/src/app/app.service.ts
+++ capstone-client/src/app/app.service.ts
@@ -2,6 +2,8 @@ import { Injectable } from '@angular/core';
 import { BehaviorSubject } from 'rxjs';
 import { Title } from '@angular/platform-browser';
 
+const LEFT_NAVBAR_EXPANDED_STATE_KEY = 'IS_LEFT_NAVBAR_EXPANDED';
+
 @Injectable({
   providedIn: 'root',
 })
@@ -16,10 +18,21 @@ export class AppService {
         pageTitle ? `${this.baseTitle} | ${pageTitle}` : this.baseTitle
       );
     });
+
+    // get previous leftnavbar state from localstorage
+    this.navbarExpanded$.next(
+      !!localStorage.getItem(LEFT_NAVBAR_EXPANDED_STATE_KEY)
+    );
   }
 
-  toggleLeftNavbar = () =>
+  toggleLeftNavbar = () => {
+    if (!this.navbarExpanded$.value) {
+      localStorage.setItem(LEFT_NAVBAR_EXPANDED_STATE_KEY, 'true');
+    } else {
+      localStorage.removeItem(LEFT_NAVBAR_EXPANDED_STATE_KEY);
+    }
     this.navbarExpanded$.next(!this.navbarExpanded$.value);
+  };
 
   setPageTitle = (pageTitle: string) => {
     this.pageTitle$.next(pageTitle);
