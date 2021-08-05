818cbf81a5e3adefc984803f50f2a465d363450e
Shevan Fernando <81354288+shevanfernando@users.noreply.github.com>
2021-08-06 00:17:10 +0530

    update AccountService

--- capstone-server/src/main/java/com/hcl/capstoneserver/account/AccountService.java
+++ capstone-server/src/main/java/com/hcl/capstoneserver/account/AccountService.java
@@ -1,48 +1,65 @@
 package com.hcl.capstoneserver.account;
 
-import com.hcl.capstoneserver.AccountStatus;
 import com.hcl.capstoneserver.account.dto.AccountVerifiedDTO;
 import com.hcl.capstoneserver.account.entity.Account;
+import com.hcl.capstoneserver.account.exception.AccountAlreadyHasUser;
 import com.hcl.capstoneserver.account.exception.AccountNotFoundException;
+import com.hcl.capstoneserver.account.exception.OTPTimedOut;
 import com.hcl.capstoneserver.account.repositories.AccountRepository;
 import com.hcl.capstoneserver.mail.sender.EmailService;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 
 import java.util.Date;
 import java.util.Objects;
 import java.util.Optional;
+import java.util.concurrent.TimeUnit;
 
 @Service
 public class AccountService {
     private final AccountRepository accountRepository;
     private final EmailService emailService;
 
+    @Value("${otp.validity.time}")
+    private Integer otpValidityTime;
+
     public AccountService(AccountRepository accountRepository, EmailService emailService) {
         this.accountRepository = accountRepository;
         this.emailService = emailService;
     }
 
-    public Boolean checkAccount(Integer accountNumber) {
+    public Boolean getOTP(Integer accountNumber) {
         Optional<Account> account = accountRepository.findById(accountNumber);
         if (account.isPresent()) {
-            emailService.send(account.get().getEmail(), account.get().getAccountNumber());
-            return true;
-        } else {
-            throw new AccountNotFoundException();
-        }
-    }
-
-    public Boolean verifiedAccount(AccountVerifiedDTO accountVerifiedDTO) {
-        Optional<Account> account = accountRepository.findById(accountVerifiedDTO.getAccountNumber());
-        if (account.isPresent()) {
-            if (account.get().getOtpExpiredDate().before(new Date())) {
-                if (Objects.equals(account.get().getOTP(), accountVerifiedDTO.getOtp())) {
-                    account.get().setAccountStatus(AccountStatus.VERIFIED);
-                    accountRepository.save(account.get());
-                    return true;
-                }
+            Account acc = account.get();
+            if (!acc.getVerified()) {
+                int OTP = emailService.send(acc.getEmail());
+                acc.setOTP(OTP);
+                acc.setOtpExpiredDate(new Date(System.currentTimeMillis() + TimeUnit.MINUTES.toMillis(otpValidityTime)));
+                accountRepository.save(acc);
+                return true;
+            } else {
+                throw new AccountAlreadyHasUser();
             }
         }
-        return false;
+
+        throw new AccountNotFoundException();
+    }
+
+    public Boolean verifyAccount(AccountVerifiedDTO accountVerifiedDTO) {
+        Optional<Account> account = accountRepository.findById(accountVerifiedDTO.getAccountNumber());
+        if (account.isPresent()) {
+            Account acc = account.get();
+            if (new Date().before(acc.getOtpExpiredDate())) {
+                if (Objects.equals(acc.getOTP(), accountVerifiedDTO.getOtp())) {
+                    acc.setVerified(true);
+                    accountRepository.save(acc);
+                    return true;
+                }
+            } else {
+                throw new OTPTimedOut();
+            }
+        }
+        throw new AccountNotFoundException();
     }
 }
