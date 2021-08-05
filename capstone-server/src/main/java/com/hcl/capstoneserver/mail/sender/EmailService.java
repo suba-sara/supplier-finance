cd775dc25df45647a14c0cc8cea7de69c677e1a0
Shevan Fernando <81354288+shevanfernando@users.noreply.github.com>
2021-08-06 00:13:01 +0530

    update EmailService

--- capstone-server/src/main/java/com/hcl/capstoneserver/mail/sender/EmailService.java
+++ capstone-server/src/main/java/com/hcl/capstoneserver/mail/sender/EmailService.java
@@ -1,7 +1,5 @@
 package com.hcl.capstoneserver.mail.sender;
 
-import com.hcl.capstoneserver.account.entity.Account;
-import com.hcl.capstoneserver.account.repositories.AccountRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.mail.javamail.JavaMailSender;
@@ -9,46 +7,32 @@ import org.springframework.mail.javamail.MimeMessageHelper;
 import org.springframework.mail.javamail.MimeMessagePreparator;
 import org.springframework.stereotype.Service;
 
-import java.util.Date;
-import java.util.Optional;
 import java.util.Random;
-import java.util.concurrent.TimeUnit;
 
 @Service
 public class EmailService {
 
     private final JavaMailSender javaMailSender;
-    private final AccountRepository accountRepository;
 
     @Value("${spring.mail.username}")
-    String emailSender;
+    private String emailSender;
 
     @Autowired
-    public EmailService(JavaMailSender javaMailSender, AccountRepository accountRepository) {
+    public EmailService(JavaMailSender javaMailSender) {
         this.javaMailSender = javaMailSender;
-        this.accountRepository = accountRepository;
     }
 
-    private Integer _generateOTP(Integer accountNumber) {
+    public Integer send(String receiver) {
         Random random = new Random();
-        int otp = 100000 + random.nextInt(900000);
-        Optional<Account> account = accountRepository.findById(accountNumber);
-        account.ifPresent(value -> {
-            value.setOTP(otp);
-            value.setOtpExpiredDate(new Date(System.currentTimeMillis() + TimeUnit.MINUTES.toMillis(10)));
-            accountRepository.save(value);
-        });
-        return otp;
-    }
-
-    public void send(String receiver, Integer accountNumber) {
+        int OTP = 100000 + random.nextInt(900000);
         MimeMessagePreparator message = mimeMessage -> {
             MimeMessageHelper messageHelper = new MimeMessageHelper(mimeMessage);
             messageHelper.setTo(receiver);
             messageHelper.setFrom(emailSender);
             messageHelper.setSubject("SHS BANK OTP CODE");
-            messageHelper.setText(String.format("Your OTP Code: %d", _generateOTP(accountNumber)));
+            messageHelper.setText(String.format("Your OTP Code: %d", OTP));
         };
         this.javaMailSender.send(message);
+        return OTP;
     }
 }
