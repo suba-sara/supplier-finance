36e5ccc0b6325723668e86ec477767e199ff4a23
Shashi Srinath <atshashisrinath@gmail.com>
2021-08-09 10:10:11 +0530

    add invoice status filter
    Signed-off-by: Shashi Srinath <atshashisrinath@gmail.com>
    
    Took 58 minutes

---
+++ capstone-client/src/app/invoice/util/getDisplayColumns.ts
@@ -0,0 +1,44 @@
+import { UserType } from '../../core/auth/auth.service';
+
+export function getDisplayColumns(userType?: UserType): string[] {
+  switch (userType) {
+    case 'CLIENT':
+      return [
+        'invoiceId',
+        'invoiceNumber',
+        'supplierId',
+        'uploadedDate',
+        'invoiceDate',
+        'invoiceAge',
+        'amount',
+        'invoiceStatus',
+        'options',
+      ];
+    case 'SUPPLIER':
+      return [
+        'invoiceId',
+        'invoiceNumber',
+        'clientId',
+        'uploadedDate',
+        'invoiceDate',
+        'invoiceAge',
+        'amount',
+        'invoiceStatus',
+      ];
+    case 'BANKER':
+      return [
+        'invoiceId',
+        'invoiceNumber',
+        'clientId',
+        'supplierId',
+        'uploadedDate',
+        'invoiceDate',
+        'invoiceAge',
+        'amount',
+        'invoiceStatus',
+        'options',
+      ];
+    default:
+      return [];
+  }
+}
