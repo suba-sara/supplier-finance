43670528842b692c5322cbf2cf83579c8777b3c1
Shevan Fernando <81354288+shevanfernando@users.noreply.github.com>
2021-08-15 16:15:15 +0530

    fixed client side tests failure

--- capstone-client/src/app/sign-up/bank-account-details-form/bank-account.service.ts
+++ capstone-client/src/app/sign-up/bank-account-details-form/bank-account.service.ts
@@ -1,23 +1,21 @@
-import {Injectable} from '@angular/core';
-import {environment} from '../../../environments/environment';
-import {HttpClient} from '@angular/common/http';
-import {Observable} from "rxjs";
+import { Injectable } from '@angular/core';
+import { environment } from '../../../environments/environment';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
 
-const {API_PATH} = environment;
+const { API_PATH } = environment;
 
 @Injectable({
   providedIn: 'root',
 })
 export class BankAccountService {
-
-  constructor(private httpClient: HttpClient) {
-  }
+  constructor(private http: HttpClient) {}
 
   public checkAccountNumber(accountNumber: any): Observable<any> {
-    return this.httpClient.get(`${API_PATH}/account/get-otp/${accountNumber}`);
+    return this.http.get(`${API_PATH}/account/get-otp/${accountNumber}`);
   }
 
   public verifyOTP(data: any): Promise<any> {
-    return this.httpClient.post(`${API_PATH}/account/verify`, data).toPromise();
+    return this.http.post(`${API_PATH}/account/verify`, data).toPromise();
   }
 }
