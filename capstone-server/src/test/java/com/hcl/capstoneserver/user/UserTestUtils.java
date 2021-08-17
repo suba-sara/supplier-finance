e378fcbd4548035d9efa7cec961505f181200681
Shevan Fernando <81354288+shevanfernando@users.noreply.github.com>
2021-08-17 18:38:52 +0530

    fixed tests

--- capstone-server/src/test/java/com/hcl/capstoneserver/user/UserTestUtils.java
+++ capstone-server/src/test/java/com/hcl/capstoneserver/user/UserTestUtils.java
@@ -1,8 +1,15 @@
 package com.hcl.capstoneserver.user;
 
+import com.hcl.capstoneserver.account.AccountService;
+import com.hcl.capstoneserver.account.repositories.AccountRepository;
 import com.hcl.capstoneserver.user.dto.BankerDTO;
+import com.hcl.capstoneserver.user.dto.ClientDTO;
+import com.hcl.capstoneserver.user.dto.PersonWithPasswordDTO;
+import com.hcl.capstoneserver.user.dto.SupplierDTO;
 import com.hcl.capstoneserver.user.entities.AppUser;
 import com.hcl.capstoneserver.user.entities.Banker;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
@@ -14,54 +21,59 @@ public class UserTestUtils {
     @Autowired
     UserService userService;
 
-    //    public List<SupplierDTO> createASupplier() {
-    //        List<SupplierDTO> suppliers = new ArrayList<>();
-    //        suppliers.add(userService.signUpSupplier(new Supplier(
-    //                "supplier",
-    //                "password",
-    //                "supplier",
-    //                "Colombo",
-    //                "supplier@gmail.com",
-    //                "071-2314531a",
-    //                2.5f
-    //        )));
-    //        suppliers.add(userService.signUpSupplier(new Supplier(
-    //                "supplier9",
-    //                "password",
-    //                "supplier2",
-    //                "Colombo",
-    //                "supplier2@gmail.com",
-    //                "071-2314531a",
-    //                2.5f
-    //        )));
-    //        return suppliers;
-    //    }
-    //
-    //    public List<ClientDTO> createAClient() {
-    //        List<ClientDTO> clients = new ArrayList<>();
-    //        clients.add(userService.signUpClient(new Client(
-    //                "client",
-    //                "password",
-    //                "client",
-    //                "Colombo",
-    //                "client@gmail.com",
-    //                "071-2314538",
-    //                2.5f,
-    //                1234567891
-    //        )));
-    //        clients.add(userService.signUpClient(new Client(
-    //                "client2",
-    //                "password",
-    //                "client2",
-    //                "Colombo",
-    //                "client2@gmail.com",
-    //                "071-2314538",
-    //                2.5f,
-    //                1234567891
-    //        )));
-    //
-    //        return clients;
-    //    }
+    @Autowired
+    AccountService accountService;
+
+    public List<SupplierDTO> createASupplier() {
+        List<SupplierDTO> suppliers = new ArrayList<>();
+        suppliers.add(userService.signUpSupplier(new PersonWithPasswordDTO(
+                "supplier",
+                "supplier",
+                "Colombo",
+                "supplier@gmail.com",
+                "071-2314531",
+                "10000235",
+                "password",
+                "482410"
+        )));
+        suppliers.add(userService.signUpSupplier(new PersonWithPasswordDTO(
+                "supplier9",
+                "supplier2",
+                "Colombo",
+                "supplier2@gmail.com",
+                "071-2314532",
+                "10000236",
+                "password",
+                "482410"
+        )));
+        return suppliers;
+    }
+
+    public List<ClientDTO> createAClient() {
+        List<ClientDTO> clients = new ArrayList<>();
+        clients.add(userService.signUpClient(new PersonWithPasswordDTO(
+                "client",
+                "client",
+                "Colombo",
+                "client@gmail.com",
+                "071-2314538",
+                "10000237",
+                "password",
+                "482410"
+        )));
+        clients.add(userService.signUpClient(new PersonWithPasswordDTO(
+                "client2",
+                "client2",
+                "Colombo",
+                "client2@gmail.com",
+                "071-2314538",
+                "10000238",
+                "password",
+                "482410"
+        )));
+
+        return clients;
+    }
 
     public List<BankerDTO> createBankers() {
         List<BankerDTO> bankers = new ArrayList<>();
