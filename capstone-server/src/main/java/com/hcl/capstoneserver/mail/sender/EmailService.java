88a5f920c75619c4924b99cdf363f985bfee3417
Shashi Srinath <atshashisrinath@gmail.com>
2021-08-17 18:40:44 +0530

    add test otp seed
    Signed-off-by: Shashi Srinath <atshashisrinath@gmail.com>
    
    Took 15 minutes

--- capstone-server/src/main/java/com/hcl/capstoneserver/mail/sender/EmailService.java
+++ capstone-server/src/main/java/com/hcl/capstoneserver/mail/sender/EmailService.java
@@ -2,15 +2,20 @@ package com.hcl.capstoneserver.mail.sender;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.core.env.Environment;
 import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.mail.javamail.MimeMessageHelper;
 import org.springframework.mail.javamail.MimeMessagePreparator;
 import org.springframework.stereotype.Service;
 
+import java.util.Arrays;
+
 @Service
 public class EmailService {
 
     private final JavaMailSender javaMailSender;
+    @Autowired
+    private Environment environment;
 
     @Value("${spring.mail.username}")
     private String emailSender;
@@ -21,6 +26,8 @@ public class EmailService {
     }
 
     public void sendOtp(String receiver, String otp) {
+        System.out.println(Arrays.toString(environment.getActiveProfiles()));
+
         MimeMessagePreparator message = mimeMessage -> {
             MimeMessageHelper messageHelper = new MimeMessageHelper(mimeMessage);
             messageHelper.setTo(receiver);
