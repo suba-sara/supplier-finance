059f283da5430ed0af3686145afd6da5de34c74a
Shevan Fernando <81354288+shevanfernando@users.noreply.github.com>
2021-08-17 19:37:04 +0530

    fixed tests

--- capstone-server/src/main/java/com/hcl/capstoneserver/seeder/DatabaseSeeder.java
+++ capstone-server/src/main/java/com/hcl/capstoneserver/seeder/DatabaseSeeder.java
@@ -38,6 +38,13 @@ public class DatabaseSeeder {
         }
     }
 
+    public void seedTest() {
+        bankerRepository.deleteAll();
+        accountRepository.deleteAll();
+        seedAccountTable();
+        seedBankerTable();
+    }
+
     private void seedBankerTable() {
         if (this.bankerRepository.count() == 0) {
             List<Banker> bankerList = new ArrayList<>();
