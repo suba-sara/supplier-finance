51887956ff9873177657647164a30421f3e7f4d4
Shevan Fernando <81354288+shevanfernando@users.noreply.github.com>
2021-08-18 06:54:15 +0530

    user service test fixed

--- capstone-server/src/test/java/com/hcl/capstoneserver/user/UserServiceTest.java
+++ capstone-server/src/test/java/com/hcl/capstoneserver/user/UserServiceTest.java
@@ -3,7 +3,9 @@ package com.hcl.capstoneserver.user;
 import com.hcl.capstoneserver.invoice.repositories.InvoiceRepository;
 import com.hcl.capstoneserver.user.dto.JwtWithTypeDTO;
 import com.hcl.capstoneserver.user.dto.PersonWithPasswordDTO;
+import com.hcl.capstoneserver.user.dto.SupplierDTO;
 import com.hcl.capstoneserver.user.entities.AppUser;
+import com.hcl.capstoneserver.user.exceptions.EmailAlreadyExistsException;
 import com.hcl.capstoneserver.user.exceptions.UserAlreadyExistsException;
 import com.hcl.capstoneserver.user.repositories.ClientRepository;
 import com.hcl.capstoneserver.user.repositories.SupplierRepository;
@@ -122,38 +124,38 @@ public class UserServiceTest {
     @Nested
     @DisplayName("Supplier Signup Tests")
     class SignUpSupplierTests {
-        //        @Test
-        //        @DisplayName("it should create a new supplier")
-        //        public void createNewSupplier() {
-        //            assertNotNull(userService.signUpSupplier(
-        //                    new PersonWithPasswordDTO(
-        //                            "sup1",
-        //                            "ma",
-        //                            "Colombo",
-        //                            "madara@konoha.org",
-        //                            "071-2314532",
-        //                            "10000239",
-        //                            "password",
-        //                            "482410"
-        //                    )));
-        //        }
+        @Test
+        @DisplayName("it should create a new supplier")
+        public void createNewSupplier() {
+            assertNotNull(userService.signUpSupplier(
+                    new PersonWithPasswordDTO(
+                            "sup1",
+                            "ma",
+                            "Colombo",
+                            "madara@konoha.org",
+                            "071-2314532",
+                            "10000239",
+                            "password",
+                            "482410"
+                    )));
+        }
 
-        //        @Test
-        //        @DisplayName("It should generate a new supplier id")
-        //        public void shouldGenerateSupplierId() {
-        //            SupplierDTO supplier = userService.signUpSupplier(new PersonWithPasswordDTO(
-        //                    "sup2",
-        //                    "ma",
-        //                    "Colombo",
-        //                    "madara2@konoha.org",
-        //                    "071-2364552",
-        //                    "10000240",
-        //                    "password",
-        //                    "482410"
-        //            ));
-        //
-        //            assertNotNull(supplier.getSupplierId());
-        //        }
+        @Test
+        @DisplayName("It should generate a new supplier id")
+        public void shouldGenerateSupplierId() {
+            SupplierDTO supplier = userService.signUpSupplier(new PersonWithPasswordDTO(
+                    "sup2",
+                    "ma",
+                    "Colombo",
+                    "madara2@konoha.org",
+                    "071-2364552",
+                    "10000239",
+                    "password",
+                    "482410"
+            ));
+
+            assertNotNull(supplier.getSupplierId());
+        }
 
         @Test
         @DisplayName("it should throw a UserAlreadyExistsException when username exists")
@@ -165,100 +167,99 @@ public class UserServiceTest {
                     "Colombo",
                     "madara21@konoha.org",
                     "071-2324552",
-                    "10000240",
+                    "10000239",
                     "password",
                     "482410"
             )));
         }
 
-        //        @Test
-        //        @DisplayName("it should throw a EmailAlreadyExistsException when username exists")
-        //        public void shouldThrowEmailAlreadyExistsException() {
-        //            userTestUtils.createASupplier();
-        //            assertThrows(EmailAlreadyExistsException.class, () -> userService.signUpSupplier(new
-        //            PersonWithPasswordDTO(
-        //                    "supplier23",
-        //                    "ma",
-        //                    "Colombo",
-        //                    "supplier@gmail.com",
-        //                    "071-2314852",
-        //                    "10000240",
-        //                    "password",
-        //                    "482410"
-        //            )));
-        //        }
-        //
+        @Test
+        @DisplayName("it should throw a EmailAlreadyExistsException when username exists")
+        public void shouldThrowEmailAlreadyExistsException() {
+            userTestUtils.createASupplier();
+            assertThrows(EmailAlreadyExistsException.class, () -> userService.signUpSupplier(new PersonWithPasswordDTO(
+                    "supplier23",
+                    "ma",
+                    "Colombo",
+                    "supplier@gmail.com",
+                    "071-2314852",
+                    "10000239",
+                    "password",
+                    "482410"
+            )));
+        }
     }
-    //
-    //
-    //    @Nested
-    //    @DisplayName("Client Signup Tests")
-    //    class SignUpClientTests {
-    //        @Test
-    //        @DisplayName("It should create new client")
-    //        public void shouldCreateNewClient() {
-    //            assertEquals(
-    //                    "Sheldon",
-    //                    userService.signUpClient(
-    //                            new Client(
-    //                                    "shel2",
-    //                                    "sdfdsfds",
-    //                                    "Sheldon",
-    //                                    "Colombo",
-    //                                    "shel@gmail.com",
-    //                                    "071-2314538",
-    //                                    2.5f,
-    //                                    1234567891
-    //                            )).getName()
-    //            );
-    //        }
-    //
-    //        @Test
-    //        @DisplayName("It should generate Client Id")
-    //        public void shouldGenerateClientId() {
-    //            assertNotNull(userService.signUpClient(new Client(
-    //                    "shel1",
-    //                    "sdfdsfds",
-    //                    "Sheldon",
-    //                    "Colombo",
-    //                    "shel1@gmail.com",
-    //                    "071-2314538",
-    //                    2.5f,
-    //                    1234567891
-    //            )).getClientId());
-    //        }
-    //
-    //        @Test
-    //        @DisplayName("it should throw a UserAlreadyExistsException when username exists")
-    //        public void shouldThrowUserAlreadyExistsException() {
-    //
-    //            assertThrows(UserAlreadyExistsException.class, () -> userService.signUpClient(new Client(
-    //                    "client",
-    //                    "sdfdsfds",
-    //                    "Sheldon",
-    //                    "Colombo",
-    //                    "shel11@gmail.com",
-    //                    "071-2314538",
-    //                    2.5f,
-    //                    1234567891
-    //            )));
-    //        }
-    //
-    //        @Test
-    //        @DisplayName("it should throw a EmailAlreadyExistsException when username exists")
-    //        public void shouldThrowEmailAlreadyExistsException() {
-    //
-    //            assertThrows(EmailAlreadyExistsException.class, () -> userService.signUpClient(new Client(
-    //                    "shel11",
-    //                    "sdfdsfds",
-    //                    "Sheldon",
-    //                    "Colombo",
-    //                    "client@gmail.com",
-    //                    "071-2314538",
-    //                    2.5f,
-    //                    1234567891
-    //            )));
-    //        }
-    //
-    //    }
+
+    @Nested
+    @DisplayName("Client Signup Tests")
+    class SignUpClientTests {
+        @Test
+        @DisplayName("It should create new client")
+        public void shouldCreateNewClient() {
+            assertEquals(
+                    "Sheldon",
+                    userService.signUpClient(
+                            new PersonWithPasswordDTO(
+                                    "shel2",
+                                    "Sheldon",
+                                    "Colombo",
+                                    "shel@gmail.com",
+                                    "071-2314538",
+                                    "10000239",
+                                    "sdfdsfds",
+                                    "482410"
+                            )).getName()
+            );
+        }
+
+        @Test
+        @DisplayName("It should generate Client Id")
+        public void shouldGenerateClientId() {
+            assertNotNull(userService.signUpClient(
+                    new PersonWithPasswordDTO(
+                            "shel1",
+                            "Sheldon",
+                            "Colombo",
+                            "shel1@gmail.com",
+                            "071-2314538",
+                            "10000239",
+                            "sdfdsfds",
+                            "482410"
+                    )).getClientId());
+        }
+
+        @Test
+        @DisplayName("it should throw a UserAlreadyExistsException when username exists")
+        public void shouldThrowUserAlreadyExistsException() {
+            assertThrows(UserAlreadyExistsException.class, () -> userService.signUpClient(
+                    new PersonWithPasswordDTO(
+                            "client",
+                            "Sheldon",
+                            "Colombo",
+                            "shel1a@gmail.com",
+                            "071-2314538",
+                            "10000239",
+                            "sdfdsfds",
+                            "482410"
+                    )));
+        }
+
+        @Test
+        @DisplayName("it should throw a EmailAlreadyExistsException when username exists")
+        public void shouldThrowEmailAlreadyExistsException() {
+
+            assertThrows(EmailAlreadyExistsException.class, () -> userService.signUpClient(
+                    new PersonWithPasswordDTO(
+                            "shel11",
+                            "Sheldon",
+                            "Colombo",
+                            "client@gmail.com",
+                            "071-2314538",
+                            "10000239",
+                            "sdfdsfds",
+                            "482410"
+                    )));
+        }
+
+    }
 }
