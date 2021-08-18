75c6386f558fddce5f80f380fe475fcde4c23582
Shashi Srinath <atshashisrinath@gmail.com>
2021-08-18 16:39:22 +0530

    make single invoice page mobile responsive
    Signed-off-by: Shashi Srinath <atshashisrinath@gmail.com>
    
    Took 51 minutes

--- capstone-client/src/app/invoice/view-single-invoice/view-single-invoice.component.ts
+++ capstone-client/src/app/invoice/view-single-invoice/view-single-invoice.component.ts
@@ -3,6 +3,7 @@ import { ActivatedRoute, Router } from '@angular/router';
 import { ViewSingleInvoicesService } from './view-single-invoice.service';
 import { Invoice } from '../invoice.types';
 import { environment } from '../../../environments/environment';
+import { AppService } from '../../app.service';
 
 @Component({
   selector: 'app-view-single-invoice',
@@ -12,32 +13,33 @@ import { environment } from '../../../environments/environment';
 export class ViewSingleInvoiceComponent implements OnInit {
   SERVER = environment.SERVER;
   invoice?: Invoice;
-  userType:any;
-  requestPayment?:boolean;
+  userType: any;
+  requestPayment?: boolean;
 
   constructor(
     private router: Router,
     private route: ActivatedRoute,
-    private viewSingleInvoicesService: ViewSingleInvoicesService
+    private viewSingleInvoicesService: ViewSingleInvoicesService,
+    private appService: AppService
   ) {
     const invoiceId = this.route.snapshot.params['id'];
-    this.viewSingleInvoicesService
-      .getInvoiceById(invoiceId).then((invoice) => {
-        this.invoice = invoice
-        this.userType = localStorage.getItem("user_type");
-        if( this.userType === "CLIENT" && this.invoice.status === "UPLOADED"){
-          this.requestPayment = true;
-        }else{
-          this.requestPayment = false;
-        }
-        console.log(this.invoice.status)
-      });
-      
+    this.viewSingleInvoicesService.getInvoiceById(invoiceId).then((invoice) => {
+      this.invoice = invoice;
+      this.userType = localStorage.getItem('user_type');
+      if (this.userType === 'CLIENT' && this.invoice.status === 'UPLOADED') {
+        this.requestPayment = true;
+      } else {
+        this.requestPayment = false;
+      }
+      console.log(this.invoice.status);
+    });
   }
 
-  ngOnInit(): void {}
+  ngOnInit(): void {
+    this.appService.setPageTitle('View Invoice');
+  }
 
-  back(){
-    this.router.navigateByUrl("invoice/view-invoices");
+  back() {
+    this.router.navigateByUrl('invoice/view-invoices');
   }
 }
