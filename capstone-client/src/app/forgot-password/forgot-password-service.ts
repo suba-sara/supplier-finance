40fa3d76d2000ad1cb4e53298a6094a93c98fee1
Shashi Srinath <atshashisrinath@gmail.com>
2021-08-18 11:57:19 +0530

    add observer type for forgot password request
    Signed-off-by: Shashi Srinath <atshashisrinath@gmail.com>
    
    Took 38 minutes

--- capstone-client/src/app/forgot-password/forgot-password-service.ts
+++ capstone-client/src/app/forgot-password/forgot-password-service.ts
@@ -1,28 +1,30 @@
-import {Injectable} from '@angular/core';
-import {HttpClient} from '@angular/common/http';
-import {Observable} from 'rxjs';
-import {environment} from '../../environments/environment';
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+import { environment } from '../../environments/environment';
 
-const {API_PATH} = environment;
+const { API_PATH } = environment;
 
 export type userUpdatePassword = {
-  userId: string,
-  password: string,
-  OTP: string
+  userId: string;
+  password: string;
+  OTP: string;
 };
 
 @Injectable({
   providedIn: 'root',
 })
 export class ForgotPasswordService {
-  constructor(private http: HttpClient) {
-  }
+  constructor(private http: HttpClient) {}
 
   searchAccount(userId: string): Observable<any> {
     return this.http.post(`${API_PATH}/user/forgotPassword/getOTP`, userId);
   }
 
-  verifyUser(data: userUpdatePassword): Observable<any> {
-    return this.http.post(`${API_PATH}/user/forgotPassword/verifyUser`, data);
+  verifyUser(data: userUpdatePassword): Observable<{ valid: true }> {
+    return this.http.post<{ valid: true }>(
+      `${API_PATH}/user/forgotPassword/verifyUser`,
+      data
+    );
   }
 }
--- capstone-client/src/app/forgot-password/forgot-password.component.ts
+++ capstone-client/src/app/forgot-password/forgot-password.component.ts
@@ -1,57 +1,74 @@
-import {Component, OnInit} from '@angular/core';
-import {AbstractControl, FormControl, FormGroup, Validators} from '@angular/forms';
-import {ForgotPasswordService} from './forgot-password-service';
+import { Component, OnInit } from '@angular/core';
+import {
+  AbstractControl,
+  FormControl,
+  FormGroup,
+  Validators,
+} from '@angular/forms';
+import { ForgotPasswordService } from './forgot-password-service';
 
 @Component({
   selector: 'app-forgot-password',
   templateUrl: './forgot-password.component.html',
-  styleUrls: ['./forgot-password.component.scss']
+  styleUrls: ['./forgot-password.component.scss'],
 })
 export class ForgotPasswordComponent implements OnInit {
-
   userAccountForm: FormGroup = new FormGroup({
-    userId: new FormControl('', Validators.required)
+    userId: new FormControl('', Validators.required),
   });
 
   errorMessages = '';
   isAccountHas = false;
 
-  constructor(private forgotPasswordService: ForgotPasswordService) {
-  }
+  constructor(private forgotPasswordService: ForgotPasswordService) {}
 
-  ngOnInit(): void {
-  }
+  ngOnInit(): void {}
 
   get userId(): AbstractControl | null {
     return this.userAccountForm.get('userId');
   }
 
   searchAccount(): void {
-    this.forgotPasswordService.searchAccount(this.userId?.value).subscribe(res => {
-      if (res) {
-        this.isAccountHas = !this.isAccountHas;
-        this.userAccountForm?.addControl(
-          'OTP',
-          new FormControl('', [
-            Validators.required,
-            Validators.maxLength(6),
-            Validators.minLength(6),
-            Validators.pattern(/^-?(0|[1-9]\d*)?$/),
-          ])
-        );
+    this.forgotPasswordService.searchAccount(this.userId?.value).subscribe(
+      (res) => {
+        if (res) {
+          this.isAccountHas = !this.isAccountHas;
+          this.userAccountForm?.addControl(
+            'OTP',
+            new FormControl('', [
+              Validators.required,
+              Validators.maxLength(6),
+              Validators.minLength(6),
+              Validators.pattern(/^-?(0|[1-9]\d*)?$/),
+            ])
+          );
 
-        this.userAccountForm?.addControl('password', new FormControl('', [Validators.required, Validators.minLength(6)]));
-        this.userAccountForm?.addControl('confirm_password', new FormControl('', [Validators.required, Validators.minLength(6)]));
-      }
-    }, err => this.errorMessages = err);
+          this.userAccountForm?.addControl(
+            'password',
+            new FormControl('', [Validators.required, Validators.minLength(6)])
+          );
+          this.userAccountForm?.addControl(
+            'confirm_password',
+            new FormControl('', [Validators.required, Validators.minLength(6)])
+          );
+        }
+      },
+      (err) => (this.errorMessages = err)
+    );
   }
 
   onSubmitButtonClick(): void {
     this.userAccountForm.get('confirm_password')?.disable();
-    this.forgotPasswordService.verifyUser(this.userAccountForm.value).subscribe(res => {
-      if (res) {
-        this.isAccountHas = !this.isAccountHas;
+    this.forgotPasswordService.verifyUser(this.userAccountForm.value).subscribe(
+      (res) => {
+        if (res.valid) {
+          this.isAccountHas = !this.isAccountHas;
+        }
+      },
+      (err) => {
+        console.log(err);
+        this.errorMessages = err;
       }
-    }, err => this.errorMessages = err);
+    );
   }
 }
